271be61b2f0b5395c5037a3911e7f92b7e6dda67
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2017-09-21 16:28:19 +0300

    Intro

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,12 +1,18 @@
 package ru.track;
 
 /**
- * Hello world!
+ * TASK:
+ * POST request to  https://guarded-mesa-31536.herokuapp.com/db
+ * fields: name,github,email
+ *
+ * LIB: http://unirest.io/java.html
+ *
+ *
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, " + args[0]);
     }
 
 }
