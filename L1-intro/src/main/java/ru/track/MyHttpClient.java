c0e92a387eac1a6441f2f5840de7a6f5c1062761
Vyacheslav Kovalev <vyacheslav.kovalev@phystech.edu>
2017-09-22 20:46:08 +0300

    minor improvements

--- L1-intro/src/main/java/ru/track/MyHttpClient.java
+++ L1-intro/src/main/java/ru/track/MyHttpClient.java
@@ -2,14 +2,17 @@ package ru.track;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class MyHttpClient {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws UnirestException {
         HttpResponse<String> r = Unirest.post("https://guarded-mesa-31536.herokuapp.com/track")
                     .field("name", "materkey")
                     .field("github", "https://github.com/materkey")
                     .field("email", "vyacheslav.kovalev@phystech.edu")
                     .asString();
-        System.out.println(r);
+        System.out.printf("code: %s; headers: %s; body: %s\n", r.getStatusText(), r.getHeaders(), r.getBody());
     }
+
 }
