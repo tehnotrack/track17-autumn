7d0e91ec2ea81b61e91ebd34244ce4723be9d6ac
mater <materkey@ubuntu.ubuntu-domain>
2017-09-22 20:07:03 +0300

    intro task commit

--- L1-intro/src/main/java/ru/track/MyHttpClient.java
+++ L1-intro/src/main/java/ru/track/MyHttpClient.java
@@ -6,9 +6,9 @@ import com.mashape.unirest.http.Unirest;
 public class MyHttpClient {
     public static void main(String[] args) throws Exception {
         HttpResponse<String> r = Unirest.post("https://guarded-mesa-31536.herokuapp.com/track")
-                    .field("name", "test ")
-                    .field("github", "test ")
-                    .field("email", "test")
+                    .field("name", "materkey")
+                    .field("github", "https://github.com/materkey")
+                    .field("email", "vyacheslav.kovalev@phystech.edu")
                     .asString();
         System.out.println(r);
     }
