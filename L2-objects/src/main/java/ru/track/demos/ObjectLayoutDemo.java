d2c6a1d1fc3f20c6faf2a6d3f11763773edb1a47
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2017-09-25 18:06:58 +0300

    L2

--- L2-objects/src/main/java/ru/track/demos/ObjectLayoutDemo.java
+++ L2-objects/src/main/java/ru/track/demos/ObjectLayoutDemo.java
@@ -24,9 +24,9 @@ public class ObjectLayoutDemo {
 //        return layout('x');
 //        return layout("helloworld");
 //        return layout(new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'}); // 12 chars! try less?
-        return layout(this.dummy);
+//        return layout(this.dummy);
 //        return layout(this);
-//        return layout(Class.forName("java.lang.String"));
+        return layout(Class.forName("java.lang.String"));
 //        return layout(Integer.parseInt("255"));
 //        return layout(Long.parseLong("DEADBEEF", 0x10));
 //        return layout(Class.class);
