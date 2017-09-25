f71295a9fd1c64c6a2b0443813e256d2d0625c32
Lizanoskova <enoskovamipt@gmail.com>
2017-09-25 18:40:15 +0300

    HW-1

--- L1-intro/src/main/java/ru/track/MyHttpClient.java
+++ L1-intro/src/main/java/ru/track/MyHttpClient.java
@@ -5,12 +5,12 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class MyHttpClient {
 
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args)throws UnirestException{
 
         Unirest.post("https://guarded-mesa-31536.herokuapp.com/track")
-                .queryString("name", "Noskova Elizaveta")
-                .queryString("github", "https://github.com/Lizanoskova")
-                .queryString("email", "enoskova.mipt@gmail.com")
+                .field("name", "Noskova Elizaveta")
+                .field("github", "https://github.com/Lizanoskova")
+                .field("email", "enoskova.mipt@gmail.com")
                 .asString();
     }
 }
