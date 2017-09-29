008c138da82c07dbc84719c2e5ee6c6c94d4f2a2
Fedor Butikov <butikov@phystech.edu>
2017-09-29 04:00:55 +0300

    Conversion to base64 implemented

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
-
 import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +20,49 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        InputStream in = new FileInputStream(finPath);
+
+        File fout;
+        if( foutPath == null) {
+            fout = File.createTempFile(finPath, "x64");
+            fout.deleteOnExit();
+        }
+        else
+            fout = new File(foutPath);
+        OutputStream out = new FileOutputStream(fout);
+        int data = in.read();
+        while (data != -1){
+            data = (data << 16);
+            int second = in.read();
+            if(second != -1){
+                data += second << 8;
+            }
+            else {
+                out.write(toBase64[(data >> 18) & 63]);
+                out.write(toBase64[(data >> 12) & 63]);
+                out.write('=');
+                out.write('=');
+                break;
+            }
+            int last = in.read();
+            if(last != -1)
+                data += last;
+            else {
+                out.write(toBase64[(data >> 18) & 63]);
+                out.write(toBase64[(data >> 12) & 63]);
+                out.write(toBase64[(data >> 6) & 63]);
+                out.write('=');
+                break;
+            }
+            out.write(toBase64[(data >> 18) & 63]);
+            out.write(toBase64[(data >> 12) & 63]);
+            out.write(toBase64[(data >> 6) & 63]);
+            out.write(toBase64[data & 63]);
+            data = in.read();
+        }
+        in.close();
+        out.close();
+        return fout;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +74,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
