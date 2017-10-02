17879b689b8bb60d08ed96ba4c5edd96b00894cd
DanielGonchar <dannygonchar@gmail.com>
2017-10-02 23:27:51 +0300

    added base64 taskimplementation

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,7 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +19,51 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        //throw new UnsupportedOperationException(); // TODO: implement
+
+        final char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+
+        File fin = new File(finPath);
+        File fout;
+
+        if (foutPath == null) {
+            fout = File.createTempFile("Base64OutputFile", ".txt");
+            fout.deleteOnExit();
+        } else {
+            fout = new File(foutPath);
+        }
+
+        InputStream is = new FileInputStream(fin);
+        BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+        StringBuilder sb = new StringBuilder();
+        int blocks = 0;
+
+        while (true) {
+            int c0 = is.read();
+            if (c0 == -1)
+                break;
+            int c1 = is.read();
+            int c2 = is.read();
+
+            int block = ((c0 & 0xFF) << 16) | ((Math.max(c1, 0) & 0xFF) << 8) | (Math.max(c2, 0) & 0xFF);
+
+            sb.append(alpha[block >> 18 & 63]);
+            sb.append(alpha[block >> 12 & 63]);
+            sb.append(c1 == -1 ? '=' : alpha[block >> 6 & 63]);
+            sb.append(c2 == -1 ? '=' : alpha[block & 63]);
+
+        }
+
+
+        PrintWriter pw = new PrintWriter( fout );
+        pw.write(sb.toString());
+        pw.close();
+
+        return fout;
+
+
+
     }
 
     private static final char[] toBase64 = {
@@ -32,9 +75,12 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();//ReferenceTaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
+        
     }
+
+
 
 }
