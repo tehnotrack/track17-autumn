4710a820ffd8c7b7b245eb9e65ddd30d4e62e43c
Vyacheslav Kovalev <vyacheslav.kovalev@phystech.edu>
2017-10-03 18:04:00 +0300

    comment only specific block

--- L2-objects/src/test/java/ru/track/CountWordsTest.java
+++ L2-objects/src/test/java/ru/track/CountWordsTest.java
@@ -1,4 +1,3 @@
-/*
 package ru.track;
 
 import java.io.BufferedReader;
@@ -7,14 +6,11 @@ import java.io.FileReader;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
-*/
 /**
  *
- *//*
-
+ */
 public class CountWordsTest {
 
     static File file;
@@ -24,24 +20,24 @@ public class CountWordsTest {
         file = new File("words.txt");
     }
 
-    @Ignore @Test
+    @Test
     public void countNumbers() throws Exception {
 
-        CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
-        long number = cw.countNumbers(file);
-        String result = cw.concatWords(file);
-
-
-        BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
-        StringBuilder expected = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            expected.append(line + " ");
-        }
-
-        Assert.assertEquals("String concat: ", expected.toString(), result);
-        Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
+//        CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
+//        long number = cw.countNumbers(file);
+//        String result = cw.concatWords(file);
+//
+//
+//        BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
+//        StringBuilder expected = new StringBuilder();
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            expected.append(line + " ");
+//        }
+//
+//        Assert.assertEquals("String concat: ", expected.toString(), result);
+//        Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
 
     }
 
-}*/
+}
