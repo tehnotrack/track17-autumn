ca1add688029841d9a7d3eca2ea823b21ff60104
S3tmefree <bloya@mail.ru>
2017-10-04 16:11:58 +0300

    Edit homework

--- L1-intro/src/main/java/ru/track/MyHttpClient.java
+++ L1-intro/src/main/java/ru/track/MyHttpClient.java
@@ -1,17 +1,13 @@
 package ru.track;
 
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.body.MultipartBody;
-import org.apache.http.HttpRequest;
 
 public class MyHttpClient {
 
     public static void main(String[] args) throws UnirestException {
         Unirest.post("https://guarded-mesa-31536.herokuapp.com/db")
-                .field("name", "Semyon")
+                .field("name", "Semyon Cheban")
                 .field("github", "https://github.com/S3tmefree")
                 .field("email", "bloya@mail.ru")
                 .asString();
