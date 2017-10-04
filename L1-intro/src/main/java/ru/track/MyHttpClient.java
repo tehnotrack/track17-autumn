3477c35b413891b65582a72aa97e06ce81c2c917
S3tmefree <bloya@mail.ru>
2017-10-04 14:54:49 +0300

    Reverted last commit

--- L1-intro/src/main/java/ru/track/MyHttpClient.java
+++ L1-intro/src/main/java/ru/track/MyHttpClient.java
@@ -1,20 +1,19 @@
 package ru.track;
 
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.body.MultipartBody;
+import org.apache.http.HttpRequest;
 
 public class MyHttpClient {
 
     public static void main(String[] args) throws UnirestException {
-        try {
-            Unirest.post("https://guarded-mesa-31536.herokuapp.com/track")
-                    .field("name", "Semjon Cheban")
-                    .field("github", "https://github.com/S3tmefree")
-                    .field("email", "bloya@mail.ru")
-                    .asString();
-        }
-        catch (UnirestException e) {
-            System.out.println("Failed");
-        }
+        Unirest.post("https://guarded-mesa-31536.herokuapp.com/db")
+                .field("name", "Semyon")
+                .field("github", "https://github.com/S3tmefree")
+                .field("email", "bloya@mail.ru")
+                .asString();
     }
 }
