711dda0da2d79031ba3e50c2aa48035633e264d8
Dmitry <dmitry.bl.5967@gmail.com>
2017-10-05 19:24:02 +0300

    L3-oop

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -11,31 +11,57 @@ import java.util.NoSuchElementException;
  */
 public class MyArrayList extends List {
 
-    public MyArrayList() {
+    int data[];
+    int size = 0; //Число элементов
+    int capacity; //Число аллоцированных элементов
+    final int DEFAULT_SIZE = 10;
 
+    public MyArrayList() {
+       this.data = new int[DEFAULT_SIZE];
+       this.capacity = DEFAULT_SIZE;
     }
 
     public MyArrayList(int capacity) {
-
+        data = new int[capacity];
+        this.capacity = capacity;
     }
 
     @Override
     void add(int item) {
-
+        if(size >= capacity) {
+            int newData[] = new int[capacity * 2 + 1];
+            System.arraycopy(data, 0, newData, 0, capacity);
+            capacity = 2 * capacity + 1;
+            data = newData;
+        }
+        data[size] = item;
+        size++;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if(idx >= size) {
+            throw new NoSuchElementException();
+        }
+        int deleted = data[idx];
+        for(int i = idx + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        data[size - 1] = 0;
+        size--;
+        return deleted;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if(idx >= size) {
+            throw new NoSuchElementException();
+        }
+        return data[idx];
     }
 
     @Override
     int size() {
-        return 0;
+        return size;
     }
 }
