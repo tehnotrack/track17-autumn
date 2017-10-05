af9e23dd124c112ce8201c9381d78f304439b917
Ilya Kompaniets <jenkslex@gmial.com>
2017-10-05 21:05:35 +0300

    Added solution for Base64 task.

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,9 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +21,45 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+
+        File fin = new File(finPath);
+
+        InputStream inputStream = new FileInputStream(fin);
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            int char_1 = inputStream.read();
+            int char_2 = inputStream.read();
+            int char_3 = inputStream.read();
+
+            if (char_1 == -1)
+                break;
+
+            int tempBuff =
+                    ((char_1 & 0xFF) << 16) |
+                    ((Math.max(char_2, 0) & 0xFF) << 8) |
+                    ((Math.max(char_3, 0) & 0xFF));
+
+            sb.append(toBase64[tempBuff >> 18 & 0x3F]);
+            sb.append(toBase64[tempBuff >> 12 & 0x3F]);
+            sb.append(char_2 == -1 ? '=' : toBase64[tempBuff >> 6 & 0x3F]);
+            sb.append(char_3 == -1 ? '=' : toBase64[tempBuff & 0x3F]);
+        }
+
+        File fout;
+        if (foutPath == null) {
+            fout = File.createTempFile("TempFile", ".txt");
+            fout.deleteOnExit();
+        } else {
+            fout = new File(foutPath);
+        }
+
+        BufferedWriter bwr = new BufferedWriter(new FileWriter(fout));
+        bwr.write(sb.toString());
+        bwr.flush();
+        bwr.close();
+
+        return fout;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +71,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
