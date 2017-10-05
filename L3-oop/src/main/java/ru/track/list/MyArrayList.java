a09f5269865bd8623a829c37a037f17ed9c0551d
karinaanton77 <karinaanton77@gmail.com>
2017-10-05 10:56:54 +0300

    update

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -1,5 +1,6 @@
 package ru.track.list;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 /**
@@ -11,31 +12,73 @@ import java.util.NoSuchElementException;
  */
 public class MyArrayList extends List {
 
-    public MyArrayList() {
+    public int[] list;
+    public int size;
+    public int cap;
 
+    public MyArrayList() {
+        int [] list = new int[10];
+        cap = 10;
+        this.size = 0;
     }
 
     public MyArrayList(int capacity) {
-
+        list = new int[capacity];
+        this.size = capacity;
+        this.cap = capacity;
     }
 
     @Override
     void add(int item) {
-
+        int[] list2 = new int[this.size + 1];
+        if (size != 0)
+            System.arraycopy(list, 0, list2, 0, size);
+        list2[size] = item;
+        list = list2;
+        size += 1;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if ((idx <  size) & (idx >= 0)){
+            if (size > 1){
+                int[] list2 = new int[size - 1];
+                int a = list[idx];
+                System.arraycopy(list, 0, list2, 0 , idx);
+                System.arraycopy(list, idx + 1, list2, idx  , size - idx - 1);
+                list = list2;
+                size--;
+                return a;
+            }
+            else {
+                size--;
+                return list[idx];
+            }
+        }
+        else throw new NoSuchElementException();
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if ((idx <  size) & (idx >= 0)){
+            return list[idx];
+        }
+        else throw new NoSuchElementException();
+
     }
 
     @Override
     int size() {
-        return 0;
+        return size;
+    }
+    public static void main(String args[])
+    {
+        MyArrayList list = new MyArrayList();
+        list.add(5);
+        list.add(8);
+        list.add(99);
+        list.remove(1);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
     }
 }
