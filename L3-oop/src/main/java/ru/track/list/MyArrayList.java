94c8722c884810a2e909a67a2987aa0a5e837444
unknown <vasili4396@mail.ru>
2017-10-05 03:45:06 +0300

    update

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -10,32 +10,67 @@ import java.util.NoSuchElementException;
  * - с аргументом - начальный размер массива
  */
 public class MyArrayList extends List {
+    int size;
+    int capacity;
+    int [] my_List;
 
     public MyArrayList() {
-
+        size = 0;
+        capacity = 10;
+        my_List = new int [capacity];
+        for (int i = 0; i < size; i++) my_List[i] = 0;
     }
 
     public MyArrayList(int capacity) {
-
+        size = capacity;
+        this.capacity = capacity;
+        my_List = new int [capacity];
+        for (int i = 0; i < size; i++) my_List[i] = 0;
     }
 
     @Override
     void add(int item) {
-
+        if (size >= capacity) {
+            capacity += 10;
+            my_List = new int [capacity];
+            my_List[size] = item;
+            size++;
+        }
+        else if (capacity == 0) {
+            capacity+=10;
+            my_List = new int [capacity];
+            my_List[size] = item;
+            size++;
+        }
+        else {
+            my_List[size] = item;
+            size++;
+        }
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx <= size) {
+            int ret = my_List[idx];
+            for (int i = size-1; i > idx; i--) {
+                my_List[i-1] = my_List[i];
+            }
+            size--;
+            return ret;
+        }
+        else throw new NoSuchElementException();
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx < size) {
+            return my_List[idx];
+        }
+        else throw new NoSuchElementException();
     }
 
     @Override
     int size() {
-        return 0;
+        return size;
     }
 }
--- L3-oop/src/main/java/ru/track/list/MyLinkedList.java
+++ L3-oop/src/main/java/ru/track/list/MyLinkedList.java
@@ -7,6 +7,9 @@ import java.util.NoSuchElementException;
  * Односвязный список
  */
 public class MyLinkedList extends List {
+    Node beg;
+    Node end;
+    int size = 0;
 
     /**
      * private - используется для сокрытия этого класса от других.
@@ -24,24 +27,63 @@ public class MyLinkedList extends List {
             this.next = next;
             this.val = val;
         }
+
+        Node(int val) {
+            this.prev = null;
+            this.next = null;
+            this.val = val;
+        }
     }
 
     @Override
     void add(int item) {
+        Node n = new Node(item);
+        if (size == 0) {
+            beg = n;
+            end = n;
+        } else {
+            end.next = n;
+            n.prev = end;
+            end = n;
+        }
+        size++;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if ((idx <= size) & (idx >= 0)) {
+            Node n = beg;
+
+            for (int i = 0; i < idx; i++) {
+                n = n.next;
+            }
+            if (idx == 0) {
+                n.next = beg;
+                n.prev = null;
+            }
+            if ((idx > 0) & (idx < size - 1)) {
+                n.prev.next = n.next;
+                n.next.prev = n.prev;
+            } else {
+                n.prev = end;
+                n.next = null;
+            }
+            size--;
+            return n.val;
+        } else throw new NoSuchElementException();
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if ((idx < size) & (idx >= 0)) {
+            Node n = beg;
+            for (int i = 0; i < idx; i++) n = n.next;
+            return n.val;
+        } else throw new NoSuchElementException();
     }
 
     @Override
     int size() {
-        return 0;
+        return size;
     }
 }
