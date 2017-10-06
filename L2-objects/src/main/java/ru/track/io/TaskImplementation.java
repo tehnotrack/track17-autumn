e066609af2222176c20632fb6a0f75f3cc8d05a4
unknown <vasili4396@mail.ru>
2017-10-07 00:48:45 +0300

    base64

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -1,13 +1,15 @@
 package ru.track.io;
 
+import com.sun.org.apache.xerces.internal.impl.io.UTF8Reader;
+import javafx.util.converter.IntegerStringConverter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Base64;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -19,8 +21,76 @@ public final class TaskImplementation implements FileEncoder {
      */
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
+
+            final File fin = new File(finPath);
+            final File fout;
+
+            if (foutPath != null) {
+                fout = new File(foutPath);
+            } else {
+                fout = File.createTempFile("based_file_", ".txt");
+                fout.deleteOnExit();
+            }
+
+            FileReader fr = new FileReader(fin);
+            BufferedReader bf = new BufferedReader(fr); // read
+
+
+            //File file = new File (foutPath);
+            PrintWriter fout1 = new PrintWriter(fout.getAbsoluteFile());
+
+            StringBuilder sb = new StringBuilder();
+
+            Integer c;
+
+            int [] arr = new int[4];
+
+            for (int i = 1; ; i++) {
+                if (i%4 != 0) {
+                    c = bf.read();
+                    System.out.println(c);
+                    if (c == -1)
+                    {
+                        if (i % 4 == 2) {
+                            sb.append ("00000000");
+                            sb.append ("00000000");
+                            for (int j = 0; j < 2; j++) {
+                                arr[j] = Integer.parseInt(sb.substring(6 * j, 6 * (j + 1)), 2);
+                                fout1.print(toBase64[arr[j]]);
+                            }
+                            fout1.print('=');fout1.print('=');
+                        }
+                        if (i % 4 == 3) {
+                            sb.append ("00000000");
+                            for (int j = 0; j < 3; j++) {
+                                arr[j] = Integer.parseInt(sb.substring(6 * j, 6 * (j + 1)), 2);
+                                fout1.print(toBase64[arr[j]]);
+                            }
+                            fout1.print('=');
+                        }
+                        break;
+                    }
+                    else {
+                        for (int k = 0; k < 8 - Integer.toBinaryString(c).length(); k++)
+                            sb.insert (8 * (i % 4 - 1) + k, "0");
+                        sb.append(Integer.toBinaryString(c));
+                    }
+
+                }
+                else {
+                    for (int j = 0; j < 4; j++) {
+                        arr[j] = Integer.parseInt(sb.substring(6 * j, 6 * (j + 1)), 2);
+                        fout1.print(toBase64[arr[j]]);
+                    }
+                    sb.setLength(0);
+                }
+            }
+
+            fout1.close();
+            return fout;
+
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        //throw new UnsupportedOperationException(); // TODO: implement
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +102,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(8000);
     }
