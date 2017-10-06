a914a7cc5720fa7f73e6945d02746cd808edef3c
murnat98 <murnat98@yandex.ru>
2017-10-06 16:41:07 +0300

    MyLinkedList and MyArrayList realized

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -11,31 +11,104 @@ import java.util.NoSuchElementException;
  */
 public class MyArrayList extends List {
 
-    public MyArrayList() {
+    private static final int DEFAULT_CAPACITY = 100;
 
+    private int[] buffer;
+    private int pointer = 0;
+    private int capacity;
+
+    public MyArrayList() {
+        this.buffer   = new int[DEFAULT_CAPACITY];
+        this.capacity = DEFAULT_CAPACITY;
     }
 
     public MyArrayList(int capacity) {
-
+        this.buffer   = new int[capacity];
+        this.capacity = capacity;
     }
 
     @Override
     void add(int item) {
+        try {
+            this.buffer[this.pointer] = item;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            ++this.capacity;
+            this.buffer = resize(this.buffer, this.capacity);
 
+            this.add(item);
+
+            return;
+        }
+
+        ++this.pointer;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (this.checkIndex(idx)) {
+            throw new NoSuchElementException();
+        }
+
+        int removedElement;
+
+        try {
+            removedElement = this.buffer[idx];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+
+        for (int i = idx; i < this.pointer - 1; ++i) {
+            this.buffer[i] = this.buffer[i + 1];
+        }
+
+        this.buffer[--this.pointer] = 0;
+
+        --this.capacity;
+
+        return removedElement;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        int result;
+
+        if (this.checkIndex(idx)) {
+            throw new NoSuchElementException();
+        }
+
+        try {
+            result = this.buffer[idx];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+
+        return result;
     }
 
     @Override
     int size() {
-        return 0;
+        return this.pointer;
+    }
+
+    private boolean checkIndex(int idx) {
+        return idx < 0 || idx > pointer - 1;
+    }
+
+    static private int[] resize(int[] source, int size) {
+        int sourceLength = source.length;
+        if (size < sourceLength) {
+            return source;
+        }
+
+        int[] result = new int[size];
+
+        for (int i = 0; i < sourceLength; ++i) {
+            result[i] = source[i];
+        }
+
+        return result;
     }
 }
