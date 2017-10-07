90784b8039e004b9b10032c6e1e43bf35990f859
PeYceBall <vorobev.iv@phystech.edu>
2017-10-07 19:02:28 +0300

    msg

--- L2-objects/src/test/java/ru/track/CountWordsTest.java
+++ L2-objects/src/test/java/ru/track/CountWordsTest.java
@@ -20,25 +20,25 @@ public class CountWordsTest {
         file = new File("words.txt");
     }
 
-@Ignore
-    @Test
-    public void countNumbers() throws Exception {
 
-        CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
-        long number = cw.countNumbers(file);
-        String result = cw.concatWords(file);
+    //@Test
+    //public void countNumbers() throws Exception {
+//
+  //      CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
+    //    long number = cw.countNumbers(file);
+      //  String result = cw.concatWords(file);
 
+//
+  //      BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
+    //    StringBuilder expected = new StringBuilder();
+     //   String line;
+      //  while ((line = reader.readLine()) != null) {
+       //     expected.append(line + " ");
+        //}
 
-        BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
-        StringBuilder expected = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            expected.append(line + " ");
-        }
+        //Assert.assertEquals("String concat: ", expected.toString(), result);
+        //Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
 
-        Assert.assertEquals("String concat: ", expected.toString(), result);
-        Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
-
-    }
+    //}
 
 }
