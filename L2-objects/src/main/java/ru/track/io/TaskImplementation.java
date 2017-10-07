57f1095d378c1072a8a788cb27de4fdcfab58e84
Sharapov Roman <sharapov.roman@gmail.com>
2017-10-07 19:07:16 +0300

    solving git probleem

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -8,6 +8,7 @@ import ru.track.io.vendor.ReferenceTaskImplementation;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -19,8 +20,49 @@ public final class TaskImplementation implements FileEncoder {
      */
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
-        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        File fin = new File(finPath);
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(fin));
+
+        File fout;
+        if (foutPath != null) {
+            fout = new File(foutPath);
+        } else {
+            fout = File.createTempFile("based_file_", ".txt");
+            fout.deleteOnExit();
+        }
+        OutputStream out = new FileOutputStream(fout);
+
+        //create buffer of three bytes
+        byte[] bufferOfThreeBytes = new byte[3];
+        int amountOfReadClips;
+
+        //read data from file by 3 bytes at the time
+        while((amountOfReadClips = input.read(bufferOfThreeBytes, 0, 3)) > 0) {
+            int clipOfThreeBytes = 0;
+            for (int counter = 0; counter < amountOfReadClips; counter++){
+                // according to the algorythm, shift iterates 16, 8, 0 in series.
+                //therefore we make bitwise OR and shift to the left(multiply by 'shift')
+                //0xff=0d255=0b11111111
+                int shift = (8 * (2 - counter));
+                clipOfThreeBytes = clipOfThreeBytes | (bufferOfThreeBytes[counter] & 0xff) << shift;
+            }
+
+            //if amountOfReadClips not equal three, we fill the rest of buffer with '='
+            char[] result = new char[]{'=', '=', '=', '='};
+            for (int counter = 0; counter < amountOfReadClips + 1; counter++) {
+                // according to the algorythm, shift iterates 18, 12, 6, 0 in series.
+                //therefore we make  shift to the right(divide by 'shift')
+                //and use it as index for search in toBase64 array
+                //0x3f=0d63=0b111111
+                int shift = 6 * (3 - counter);
+                result[counter] = toBase64[clipOfThreeBytes >> shift & 0x3f];
+               // out.write(result[counter]);
+            }
+
+            out.write(new String(result).getBytes());
+
+        }
+        return fout;
     }
 
     private static final char[] toBase64 = {
@@ -32,8 +74,8 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
-        // NOTE: open http://localhost:9000/ in your web browser
+        final FileEncoder encoder = new TaskImplementation();
+        // NOTE: open http://loca lhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
 
