c8aa85bcd79e26fd3c86c10f983ecea73e7c4606
DemidovAlexander <demidov-study@yandex.ru>
2017-10-08 01:34:22 +0300

    Added base64encoder implementation

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,10 +6,42 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 public final class TaskImplementation implements FileEncoder {
+
+    private int toUnsignedByte(byte value) {
+        return value & 0xFF;
+    }
+
+    private void encode(FileInputStream inputStream, BufferedWriter outputWriter) throws IOException {
+        byte[] buffer = new byte[3];
+
+        int nRead;
+        while((nRead = inputStream.read(buffer)) != -1) {
+            int fullBitMask = toUnsignedByte(buffer[0]);
+
+            StringBuilder reversedResultBuilder = new StringBuilder();
+
+            for (int i = 1; i < 3; i++) {
+                fullBitMask = fullBitMask * 256 + toUnsignedByte(buffer[i]);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                if (nRead < 3 && i == 0 || nRead == 1 && i == 1) {
+                    reversedResultBuilder.append("=");
+                } else {
+                    reversedResultBuilder.append(toBase64[fullBitMask % 64]);
+                }
+
+                fullBitMask >>>= 6;
+            }
+
+            outputWriter.write(reversedResultBuilder.reverse().toString());
+            Arrays.fill(buffer, (byte)0);
+        }
+    }
 
     /**
      * @param finPath  where to read binary data from
@@ -19,8 +51,23 @@ public final class TaskImplementation implements FileEncoder {
      */
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
-        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        final File inputFile = new File(finPath);
+        final File outputFile;
+
+        if (foutPath == null) {
+            outputFile = File.createTempFile("tem", ".txt");
+            outputFile.deleteOnExit();
+        } else {
+            outputFile = new File(foutPath);
+        }
+
+        try (FileInputStream inputStream = new FileInputStream(inputFile);
+             BufferedWriter outputWriter = new BufferedWriter(new FileWriter(outputFile))) {
+
+            encode(inputStream, outputWriter);
+        }
+
+        return outputFile;
     }
 
     private static final char[] toBase64 = {
@@ -32,9 +79,9 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
+
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
-
 }
