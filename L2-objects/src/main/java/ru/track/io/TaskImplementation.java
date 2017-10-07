c468519c44aed3dbd5039736b1e26a85a4bcb613
nLoro <Rudenko.N@outlook.com>
2017-10-07 19:01:50 +0300

    a

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -4,65 +4,23 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
+import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.*;
-public final class TaskImplementation implements FileEncoder
-{
+import java.io.File;
+import java.io.IOException;
 
+public final class TaskImplementation implements FileEncoder {
 
-    private static int toUnsigned(byte[] buffer)
-    {
-        int unsigned =0;
-        for(int i=0;i<3;++i)
-        {
-            int mid;
-            if(buffer[i]<0) mid=(buffer[i]&127)+128;
-            else mid=buffer[i];
-            mid=mid<<((2-i)*8);
-            unsigned+=mid;
-        }
-        return unsigned;
-    }
-
+    /**
+     * @param finPath  where to read binary data from
+     * @param foutPath where to write encoded data. if null, please create and use temporary file.
+     * @return file to read encoded data from
+     * @throws IOException is case of input/output errors
+     */
     @NotNull
-    public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException
-    {
-        final File fin = new File(finPath);
-        final File fout;
-
-        if (foutPath != null)
-        {
-            fout = new File(foutPath);
-        }
-        else
-        {
-            fout = File.createTempFile("based_file_", ".txt");
-            fout.deleteOnExit();
-        }
-        FileInputStream fis = new FileInputStream(fin);
-        OutputStream os = new FileOutputStream(fout);
-
-        int s;
-        byte[] buffer=new byte[3];
-        s=fis.read(buffer);
-        int unsigned = toUnsigned(buffer);
-        int index;
-        while (s>=0)
-        {
-            for(int i=0;i<s+1;++i)
-            {
-                index=((unsigned<<(i*6+8))>>(18+8))&63;
-                os.write(toBase64[index]);
-            }
-            for(int i=0;i<3-s;++i)
-            {
-                os.write('=');
-            }
-            for(int i=0;i<3;++i) buffer[i]=0;
-            s=fis.read(buffer);
-            unsigned=toUnsigned(buffer);
-        }
-        return fout;
+    public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
+        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
+        throw new UnsupportedOperationException(); // TODO: implement
     }
 
     private static final char[] toBase64 = {
@@ -73,10 +31,10 @@ public final class TaskImplementation implements FileEncoder
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
     };
 
-    public static void main(String[] args) throws IOException
-    {
-        final FileEncoder encoder = new TaskImplementation();
-//         NOTE: open http://localhost:9000/ in your web browser
+    public static void main(String[] args) throws IOException {
+        final FileEncoder encoder = new ReferenceTaskImplementation();
+        // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
+
 }
