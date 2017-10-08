3a8957ac7e42ed5623e555bd10cf19bdd42e6f12
Dmitry <dmitry.bl.5967@gmail.com>
2017-10-08 22:57:28 +0300

    task base64

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -1,13 +1,14 @@
 package ru.track.io;
 
+import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +21,55 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+
+        File fileToRead = new File(finPath);
+        File fileToWrite;
+
+        if (foutPath != null) {
+            fileToWrite = new File(foutPath);
+        } else {
+             fileToWrite = File.createTempFile("tmp", ".txt");
+             fileToWrite.deleteOnExit();
+        }
+
+        InputStream in = new FileInputStream(fileToRead);
+        OutputStream out = new FileOutputStream(fileToWrite);
+        byte[] bytes = new byte[3];
+
+        int n = 0;
+            while ((n = in.read(bytes, 0, 3)) > 0) {
+                byte[] b = new byte[n];
+
+                //n - это число байт, которые удалось считать ( их может быть меньше 3)
+                for (int i = 0; i < n; i++) {
+                    b[i] = bytes[i];
+                }
+
+                int dex = 0;
+                for(int i = b.length - 1; i >= 0; i--) {
+                    //Запись (b[i] & 0xff) возвращает нам int(содержащий 32 бита)
+                    //в котором 8 бит это b[i], а остальные нули
+                    //эти 8 бит нужно сдвинуть влево в зависимости от номера байта
+                    dex |= ((b[b.length - 1 - i] & 0xff) << (8 * i));
+                }
+
+                //Получили в dex int, содержащий все наши байты на своих позициях
+                for (int i = 0; i < n + 1; i++) {
+                    // Находим байт с нужным индексом и наклыдываем на байт маску 111111 = 63
+                    // маска соответствует 6 битам
+                    // чтобы получить int, соответствующий индексу в массиве символов
+                    out.write(toBase64[0x3f & (dex >> 6 * (3 - i))]);
+                }
+                if (n == 1) {
+                    out.write('=');
+                    out.write('=');
+                }
+                if (n == 2) {
+                    out.write('=');
+                }
+            }
+
+        return fileToWrite;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +81,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
