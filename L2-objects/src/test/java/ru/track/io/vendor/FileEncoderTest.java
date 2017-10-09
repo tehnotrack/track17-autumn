d873c043da3cbb74393a96a567045b31665e5227
Maxim Galaganov <m.galaganov@corp.mail.ru>
2017-10-10 02:10:36 +0300

    L2: improve base64 test cases

--- L2-objects/src/test/java/ru/track/io/vendor/FileEncoderTest.java
+++ L2-objects/src/test/java/ru/track/io/vendor/FileEncoderTest.java
@@ -3,6 +3,7 @@ package ru.track.io.vendor;
 import junit.framework.TestCase;
 import junitx.framework.FileAssert;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,21 +28,32 @@ import static junitx.framework.FileAssert.assertBinaryEquals;
 public class FileEncoderTest extends TestCase {
 
     @Parameters
-    public static Collection<String> data() {
+    public static Collection<Sample> data() {
         return Arrays.asList(
-                "xxx",
-                "xx",
-                "x",
-                "xxx000",
-                "xxx00",
-                "xxx0"
+                new Sample("xxx"),
+                new Sample("xx"),
+                new Sample("x"),
+                new Sample("xxx000"),
+                new Sample("xxx00"),
+                new Sample("xxx0"),
+                /* edge cases for 4 KiB buffer */
+                new Sample(StringUtils.repeat("x", 4097)),
+                new Sample(StringUtils.repeat("x", 4098)),
+                new Sample(StringUtils.repeat("x", 4099)),
+                /* edge cases for 8 KiB buffer */
+                new Sample(StringUtils.repeat("x", 8193)),
+                new Sample(StringUtils.repeat("x", 8194)),
+                new Sample(StringUtils.repeat("x", 8195)),
+                /* jpeg header -- mixed endianness */
+                new Sample(new byte[]{(byte) 0xd8, (byte) 0xff, (byte) 0xe0, (byte) 0xff}),
+                new Sample(new byte[]{(byte) 0xff, (byte) 0xd8, (byte) 0xff, (byte) 0xe0})
         );
     }
 
     @NotNull
-    private final String data;
+    private final Sample data;
 
-    public FileEncoderTest(@NotNull String data) {
+    public FileEncoderTest(@NotNull Sample data) {
         this.data = data;
     }
 
@@ -49,24 +61,54 @@ public class FileEncoderTest extends TestCase {
     public void testEncoderImplementation() throws Exception {
         final Path p = Files.write(
                 createTempFile().toPath(),
-                data.getBytes(StandardCharsets.US_ASCII),
+                data.get(),
                 StandardOpenOption.WRITE
         );
 
         final File expected = (new ReferenceTaskImplementation()).encodeFile(p.toString(), null);
         final File actual = (new TaskImplementation()).encodeFile(p.toString(), null);
 
-        assertEquals(
-                FileUtils.readFileToString(expected, StandardCharsets.US_ASCII),
-                FileUtils.readFileToString(actual, StandardCharsets.US_ASCII)
-        );
+        if (!data.isPrintable()) {
+            assertBinaryEquals(expected, actual);
+        } else {
+            assertEquals(
+                    FileUtils.readFileToString(expected, StandardCharsets.US_ASCII),
+                    FileUtils.readFileToString(actual, StandardCharsets.US_ASCII)
+            );
+        }
     }
 
-    private static @NotNull
-    File createTempFile() throws IOException {
+    @NotNull
+    private static File createTempFile() throws IOException {
         final File f = File.createTempFile("test_tempfile_", ".tmp");
         f.deleteOnExit();
         return f;
+    }
+
+
+    private static class Sample {
+
+        private final byte[] raw;
+        private final String printable;
+
+        Sample(@NotNull byte[] raw) {
+            this.raw = raw;
+            this.printable = null;
+        }
+
+        Sample(@NotNull String printable) {
+            this.printable = printable;
+            this.raw = null;
+        }
+
+        byte[] get() {
+            return printable != null ? printable.getBytes() : raw;
+        }
+
+        boolean isPrintable() {
+            return printable != null;
+        }
+
     }
 
 }
