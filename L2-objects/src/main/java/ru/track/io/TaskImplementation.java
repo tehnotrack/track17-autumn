7adc321271c9d9ab3b186071e9707f7d46256e76
AtlasKirill <akimowkirill@gmail.com>
2017-10-09 23:44:17 +0300

    Task was corrected

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -7,6 +7,8 @@ import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -16,65 +18,56 @@ public final class TaskImplementation implements FileEncoder {
      * @return file to read encoded data from
      * @throws IOException is case of input/output errors
      */
+
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        //throw new UnsupportedOperationException(); // TODO: implement
 
-        StringBuilder data = new StringBuilder();
-        File out_file;
+        FileInputStream fis = new FileInputStream(finPath);
+        File outFile;
 
         if (foutPath == null) {
-            out_file = File.createTempFile("EncodedFile", ".txt");
-            out_file.deleteOnExit();
+            outFile = File.createTempFile("EncodedFile", ".txt");
+            outFile.deleteOnExit();
         } else {
-            out_file = new File(foutPath);
+            outFile = new File(foutPath);
         }
 
-        try (BufferedReader br = new BufferedReader(new FileReader(finPath))) {
-            // reading byte by byte and adding the result to string
-            int b;
-            while ((b = br.read()) != -1) {
-                data.append((char) b);
+
+        try (BufferedInputStream bis = new BufferedInputStream(fis); FileWriter fw = new FileWriter(outFile)) {
+
+            byte[] data = new byte[3];
+            int counter = 0;
+
+            while ((counter = bis.read(data)) != -1) {
+                //byte[] output = new byte[4];
+                String r = "", p = "";
+                if (counter == 3) {
+                    // these three 8-bit (ASCII) characters become one 24-bit number
+                    int n = (data[0] << 16) + (data[1] << 8) + (data[2]);
+                    // this 24-bit number gets separated into four 6-bit numbers
+                    int n1 = (n >> 18) & 63, n2 = (n >> 12) & 63, n3 = (n >> 6) & 63, n4 = n & 63;
+                    r += "" + toBase64[n1] + toBase64[n2]
+                            + toBase64[n3] + toBase64[n4];
+                }
+                if (counter < 3) {
+                    for (int i = counter; i >= counter; i--)
+                        data[i] = 0;
+                    int n = (data[0] << 16) + (data[1] << 8) + (data[2]);
+                    for (int i = 0; i <= counter; i++) {
+                        int ni = (n >> 18 - 6 * i) & 63;
+                        r += "" + toBase64[ni];
+                    }
+                    for (; counter < 3; counter++)
+                        p += "=";
+                    r += p;
+                }
+                fw.write(r);
+
             }
-        } catch (IOException e) {
-            System.out.print(e.getMessage());
+
         }
-
-        String r = "", p = "";
-        int c = data.length() % 3;
-
-        if (c > 0) {
-            for (; c < 3; c++) {
-                p += "=";
-                data.append("\0");
-            }
-        }
-
-        for (c = 0; c < data.length(); c += 3) {
-
-            // we add newlines after every 76 output characters, according to
-            // the MIME specs
-            if (c > 0 && (c / 3 * 4) % 76 == 0)
-                r += "\r\n";
-
-            // these three 8-bit (ASCII) characters become one 24-bit number
-            int n = (data.charAt(c) << 16) + (data.charAt(c + 1) << 8)
-                    + (data.charAt(c + 2));
-
-            // this 24-bit number gets separated into four 6-bit numbers
-            int n1 = (n >> 18) & 63, n2 = (n >> 12) & 63, n3 = (n >> 6) & 63, n4 = n & 63;
-
-            // those four 6-bit numbers are used as indices into the base64
-            // character list
-            r += "" + toBase64[n1] + toBase64[n2]
-                    + toBase64[n3] + toBase64[n4];
-        }
-        PrintWriter pw = new PrintWriter(out_file);
-        pw.write(r.substring(0, r.length() - p.length()) + p);
-        pw.close();
-
-        return out_file;
+        return outFile;
     }
 
     private static final char[] toBase64 = {
