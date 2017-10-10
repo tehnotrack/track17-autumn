44ff978bd1f503becc9e9e39ccadb397d06e01ad
NifantovaIrina <nifantova98@gmail.com>
2017-10-10 19:42:52 +0300

    Base64-encoder

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,12 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -17,10 +21,159 @@ public final class TaskImplementation implements FileEncoder {
      * @return file to read encoded data from
      * @throws IOException is case of input/output errors
      */
+
+    void encode1Bytes(byte b1, ArrayList<Character> res) {
+        byte temp = 0;
+        byte pow = 1;
+        for (int j = 4; j <= 128; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+
+        }
+        res.add(toBase64[temp]);
+        temp = 0;
+        pow = 16;
+        for (int j = 1; j <= 2; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+        res.add('=');
+        res.add('=');
+    }
+    void encode2Bytes(byte b1, byte b2, ArrayList<Character> res) {
+        byte temp = 0;
+        byte pow = 1;
+        for (int j = 4; j <= 128; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+
+        }
+        res.add(toBase64[temp]);
+
+        temp = 0;
+        pow = 16;
+        for (int j = 1; j <= 2; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        pow = 1;
+        for (int j = 16; j <= 128; j *= 2) {
+            if ((j & b2) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+
+        temp = 0;
+        pow = 4;
+        for (int j = 1; j <= 8; j *= 2) {
+            if ((j & b2) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+        res.add('=');
+    }
+
+    void encode3Bytes(byte b1, byte b2, byte b3, ArrayList<Character> res) {
+        byte temp = 0;
+        byte pow = 1;
+        for (int j = 4; j <= 128; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+
+        }
+        res.add(toBase64[temp]);
+
+        temp = 0;
+        pow = 16;
+        for (int j = 1; j <= 2; j *= 2) {
+            if ((j & b1) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        pow = 1;
+        for (int j = 16; j <= 128; j *= 2) {
+            if ((j & b2) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+
+        temp = 0;
+        pow = 4;
+        for (int j = 1; j <= 8; j *= 2) {
+            if ((j & b2) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        pow = 1;
+        for (int j = 64; j <= 128; j *=2) {
+            if ((j & b3) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+
+        temp = 0;
+        pow = 1;
+        for (int j = 1; j <= 32; j *=2) {
+            if ((j & b3) != 0) {
+                temp += pow;
+            }
+            pow *= 2;
+        }
+        res.add(toBase64[temp]);
+    }
+
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
-        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        Path path;
+        try {
+            path = Paths.get(finPath);
+        }
+        catch (InvalidPathException name) {
+            throw new IOException("Having exception " + name);
+        }
+
+        byte[] bytes = Files.readAllBytes(path);
+
+        ArrayList<Character> res = new ArrayList<>();
+        for (int i = 0; i < bytes.length - 2; i += 3) {
+            encode3Bytes(bytes[i], bytes[i + 1], bytes[i + 2], res);
+        }
+        if (bytes.length % 3 == 1) {
+            encode1Bytes(bytes[bytes.length - 1], res);
+        } else if (bytes.length % 3 == 2) {
+            encode2Bytes(bytes[bytes.length - 2], bytes[bytes.length - 1], res);
+        }
+        if (foutPath == null) {
+            foutPath = "./res.txt";
+        }
+        FileOutputStream outputstream = new FileOutputStream(foutPath);
+        for (int i = 0; i < res.size(); i++) {
+            outputstream.write(res.get(i));
+        }
+        outputstream.close();
+        File file = new File(foutPath);
+        return file;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +185,9 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        System.out.println(args.length);
+        System.out.println(args[0]);
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
