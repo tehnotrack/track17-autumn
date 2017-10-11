5d4d3066d4d87ec3f46c5e3dcae77d79332b49cb
Dmitriy Goryachiy <goryachii@phystech.edu>
2017-10-11 16:38:05 +0300

    не выводит картинку

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,7 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -19,8 +18,49 @@ public final class TaskImplementation implements FileEncoder {
      */
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
-        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        final File fin = new File(finPath);
+        final File fout;
+
+        if (foutPath != null) {
+            fout = new File(foutPath);
+        } else {
+            fout = File.createTempFile("based_file_", ".txt");
+            fout.deleteOnExit();
+        }
+
+        try (BufferedInputStream reader = new BufferedInputStream(new FileInputStream(fin)); FileWriter wrighter = new FileWriter(fout)) {
+            byte b1, b2, b3;
+            while (true) {
+                b1 = (byte) reader.read();
+                b2 = (byte) reader.read();
+                b3 = (byte) reader.read();
+
+                if (b1 == -1) {
+                    break;
+                }
+                int signs;
+                if (b2 == -1) {
+                    signs = ((b1 & 0x3F) << 16);
+                } else if (b3 == -1) {
+                    signs = ((b1 & 0x3F) << 16) | ((b2 & 0x3F) << 8);
+                } else {
+                    signs = ((b1 & 0x3F) << 16) | ((b2 & 0x3F) << 8) | (b3 & 0x3F);
+                }
+                wrighter.write(toBase64[signs >> 18 & 63]);
+                wrighter.write(toBase64[signs >> 12 & 63]);
+                if (b2 == -1 && b3 == -1) {
+                    wrighter.write('=');
+                    wrighter.write('=');
+                } else if (b2 != -1 && b3 == -1) {
+                    wrighter.write(toBase64[signs >> 6 & 63]);
+                    wrighter.write('=');
+                } else {
+                    wrighter.write(toBase64[signs >> 6 & 63]);
+                    wrighter.write(toBase64[signs & 63]);
+                }
+            }
+        }
+        return fout;
     }
 
     private static final char[] toBase64 = {
@@ -32,8 +72,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
-        // NOTE: open http://localhost:9000/ in your web browser
+        final FileEncoder encoder = new TaskImplementation();
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
 
