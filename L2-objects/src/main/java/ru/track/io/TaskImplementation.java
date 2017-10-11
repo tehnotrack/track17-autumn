fc2b215d3d164edc56c9bed69456f2b7acb8aa1f
liza22 <liza-boyarkina@yandex.ru>
2017-10-11 23:39:53 +0300

    немного не доделано

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,7 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +19,56 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        final File fin = new File(finPath);
+        final File fout;
+
+        if (foutPath != null) {
+            fout = new File(foutPath);
+        } else {
+            fout = File.createTempFile("based_file_", ".txt");
+            fout.deleteOnExit();
+        }
+
+        int count;
+        FileInputStream fis = null;
+        FileOutputStream os = null;
+        try {
+            fis = new FileInputStream(fin);
+            os = new FileOutputStream(fout);
+            byte[] data = new byte[3];
+
+            while ((count = fis.read(data)) != -1) {
+                os.write(convert3bytes(data, count));
+            }
+
+        } finally {
+            if (fis != null) {
+                   fis.close();
+            }
+        }
+
+
+        return fout;
+    }
+
+    private static byte[] convert3bytes(byte[] buf, int count) {
+        int bytesTogether;
+        if (count == 3)
+            bytesTogether = buf[2] + (buf[1] << 8) + (buf[0] << 16);
+        else if(count == 2)
+            bytesTogether = (buf[1] << 8) + (buf[0] << 16);
+        else if(count == 1)
+            bytesTogether = buf[0] << 16;
+
+        for (int i = 3; i >= zeros; --i) {
+            res.append(toBase64[(value >> (i * 6)) & 0x3f]);
+        }
+        for (int i = 0; i < zeros; ++i) {
+            res.append("=");
+        }
+        (value >> (i * 6)) & 0x3f]
+
+        return buf;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +80,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
