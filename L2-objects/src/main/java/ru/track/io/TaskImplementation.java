3d5426ceb888f69f20b5d306101c7a436d1070c1
murnat98 <murnat98@yandex.ru>
2017-10-11 16:52:25 +0300

    base64 done

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -3,13 +3,60 @@ package ru.track.io;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
-import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
+import ru.track.io.vendor.FileEncoder;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public final class TaskImplementation implements FileEncoder {
+    private static final int BASE64_ITERATION = 3;
+    private static final int BYTE_COUNT = 4;
+    private static final int ONES = 63;
+
+    @NotNull
+    private String fillBuffer(byte[] string, int count) {
+        int buffer = 0;
+        int void_symbols_count = BASE64_ITERATION - count;
+
+        buffer |= ((string[0] & 0xFF) << 16);
+        buffer |= ((string[1] & 0xFF) << 8);
+        buffer |= ((string[2] & 0xFF));
+
+        int[] bytes = new int[BYTE_COUNT];
+        bytes[0] = ONES & (buffer >> 18);
+        bytes[1] = ONES & (buffer >> 12);
+        bytes[2] = ONES & (buffer >> 6);
+        bytes[3] = (ONES & buffer);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < BYTE_COUNT - void_symbols_count; ++i) {
+            result.append(toBase64[bytes[i]]);
+        }
+
+        for (int i = 0; i < void_symbols_count; ++i) {
+            result.append('=');
+        }
+
+        return result.toString();
+    }
+
+    @NotNull
+    private String Base64Encode(FileInputStream file) throws IOException {
+        StringBuilder result = new StringBuilder();
+
+        byte[] bytes = new byte[BASE64_ITERATION];
+        int count;
+
+        while ((count = file.read(bytes, 0, 3)) != -1) {
+            result.append(fillBuffer(bytes, count));
+        }
+
+        return result.toString();
+    }
 
     /**
      * @param finPath  where to read binary data from
@@ -20,7 +67,29 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+
+        File fout;
+
+        if (foutPath != null) {
+            fout = new File(foutPath);
+        } else {
+            fout = File.createTempFile("based_file_", ".txt");
+            fout.deleteOnExit();
+        }
+
+        File fin = new File(finPath);
+
+        try (
+                FileInputStream finStream = new FileInputStream(fin);
+                PrintWriter foutStream = new PrintWriter(fout)
+        ) {
+            String result = Base64Encode(finStream);
+
+            foutStream.print(result);
+        }
+
+
+        return fout;
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +101,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
