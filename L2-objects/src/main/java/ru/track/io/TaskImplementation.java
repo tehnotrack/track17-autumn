dd050c5a8460bbc78f13f0c65d635bd6d3becaaa
Grigory <mrgrigorii@Jjo.Dlink>
2017-10-11 14:46:20 +0300

    Base64 Kovtun Grigory

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -5,9 +5,11 @@ import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -20,7 +22,63 @@ public final class TaskImplementation implements FileEncoder {
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+        final File fin = new File(finPath);
+        final File fout = this.getOutFile(foutPath);
+        encodeToBase64(fin, fout);
+        return fout;
+    }
+
+    @NotNull
+    private File getOutFile(@Nullable String foutPath) throws  IOException {
+        final File fout;
+
+        if (foutPath != null) {
+            fout = new File(foutPath);
+        } else {
+            fout = File.createTempFile("based_file_", ".txt");
+            fout.deleteOnExit();
+        }
+        return fout;
+    }
+
+    private static void encodeToBase64(File fin, File fout) throws IOException{
+        int bufferIn;
+        int bufferSize = 3;
+        byte[] bytes;
+        bytes = new byte[bufferSize];
+
+        try(FileInputStream fis = new FileInputStream(fin);
+            FileOutputStream fous = new FileOutputStream(fout)) {
+            do {
+                Arrays.fill(bytes, (byte) 0);
+                bufferIn = fis.read(bytes);
+                int buffer = ((bytes[0] & 0xff) << 16) + ((bytes[1] & 0xff) << 8) + ((bytes[2] & 0xff));
+                writeBufferData(buffer, bufferIn, fous);
+            } while (bufferIn != -1);
+        }
+    }
+
+    private static void writeBufferData(int buffer, int bufferIn, FileOutputStream fous) throws IOException{
+        if (bufferIn == 3) {
+            convertAndWrite(buffer, 18, fous);
+            convertAndWrite(buffer, 12, fous);
+            convertAndWrite(buffer, 6, fous);
+            convertAndWrite(buffer, 0, fous);
+        } else if (bufferIn == 2) {
+            convertAndWrite(buffer, 18, fous);
+            convertAndWrite(buffer, 12, fous);
+            convertAndWrite(buffer, 6, fous);
+            fous.write('=');
+        } else if (bufferIn == 1) {
+            convertAndWrite(buffer, 18, fous);
+            convertAndWrite(buffer, 12, fous);
+            fous.write('=');
+            fous.write('=');
+        }
+    }
+
+    private static void convertAndWrite(int buffer, int shift, FileOutputStream fous) throws IOException{
+        fous.write(toBase64[((buffer >> shift) & 0x3f)]);
     }
 
     private static final char[] toBase64 = {
@@ -32,7 +90,7 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
