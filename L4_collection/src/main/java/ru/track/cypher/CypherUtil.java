8e6ab97b04bf9653fc22f2c46b392c155a20e285
Andrew Gonchar <biohazard100000@gmail.com>
2017-10-12 20:41:03 +0300

    Encoder finished.

--- L4_collection/src/main/java/ru/track/cypher/CypherUtil.java
+++ L4_collection/src/main/java/ru/track/cypher/CypherUtil.java
@@ -1,6 +1,6 @@
 package ru.track.cypher;
 
-import java.util.Map;
+import java.util.*;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -19,7 +19,16 @@ public class CypherUtil {
      */
     @NotNull
     public static Map<Character, Character> generateCypher() {
-        return null;
+       Map<Character, Character> cypherTable = new TreeMap<>();
+       List<Character> shuffled = new ArrayList<>();
+        for (int i = 0; i < SYMBOLS.length(); i++) {
+            shuffled.add(SYMBOLS.charAt(i));
+        }
+        Collections.shuffle(shuffled);
+        for (int i = 0; i < SYMBOLS.length(); i++) {
+            cypherTable.put(SYMBOLS.charAt(i), shuffled.get(i));
+        }
+        return cypherTable;
     }
 
 }
