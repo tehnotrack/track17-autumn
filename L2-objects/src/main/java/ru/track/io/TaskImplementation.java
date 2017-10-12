5a20887ab4e6c36b73caa83cc1cd2355e5611a52
murnat98 <murnat98@yandex.ru>
2017-10-12 16:45:58 +0300

    base64 done

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -9,54 +9,13 @@ import ru.track.io.vendor.FileEncoder;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public final class TaskImplementation implements FileEncoder {
     private static final int BASE64_ITERATION = 3;
     private static final int BYTE_COUNT = 4;
     private static final int ONES = 63;
-
-    @NotNull
-    private String fillBuffer(byte[] string, int count) {
-        int buffer = 0;
-        int void_symbols_count = BASE64_ITERATION - count;
-
-        buffer |= ((string[0] & 0xFF) << 16);
-        buffer |= ((string[1] & 0xFF) << 8);
-        buffer |= ((string[2] & 0xFF));
-
-        int[] bytes = new int[BYTE_COUNT];
-        bytes[0] = ONES & (buffer >> 18);
-        bytes[1] = ONES & (buffer >> 12);
-        bytes[2] = ONES & (buffer >> 6);
-        bytes[3] = (ONES & buffer);
-
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < BYTE_COUNT - void_symbols_count; ++i) {
-            result.append(toBase64[bytes[i]]);
-        }
-
-        for (int i = 0; i < void_symbols_count; ++i) {
-            result.append('=');
-        }
-
-        return result.toString();
-    }
-
-    @NotNull
-    private String Base64Encode(FileInputStream file) throws IOException {
-        StringBuilder result = new StringBuilder();
-
-        byte[] bytes = new byte[BASE64_ITERATION];
-        int count;
-
-        while ((count = file.read(bytes, 0, 3)) != -1) {
-            result.append(fillBuffer(bytes, count));
-        }
-
-        return result.toString();
-    }
 
     /**
      * @param finPath  where to read binary data from
@@ -81,11 +40,29 @@ public final class TaskImplementation implements FileEncoder {
 
         try (
                 FileInputStream finStream = new FileInputStream(fin);
-                PrintWriter foutStream = new PrintWriter(fout)
+                FileOutputStream foutStream = new FileOutputStream(fout)
         ) {
-            String result = Base64Encode(finStream);
+            int count, buffer;
+            byte[] bytes = new byte[BASE64_ITERATION];
+            byte[] initResult = new byte[BYTE_COUNT];
 
-            foutStream.print(result);
+            while ((count = finStream.read(bytes, 0, 3)) != -1) {
+                buffer = 0;
+
+                for (int i = 0; i < count; ++i) {
+                    buffer |= ((bytes[i] & 0xFF) << 8 * (2 - i));
+                }
+
+                for (int i = 0; i <= count; ++i) {
+                    initResult[i] = (byte) toBase64[ONES & (buffer >> (6 * (3 - i)))];
+                }
+
+                for (int i = count + 1; i < BYTE_COUNT; ++i) {
+                    initResult[i] = (byte) '=';
+                }
+
+                foutStream.write(initResult);
+            }
         }
 
 
