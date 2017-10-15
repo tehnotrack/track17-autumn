864c38f5d87f8b5e2050975bc1aa58001cce5cf8
nLoro <Rudenko.N@outlook.com>
2017-10-15 18:17:43 +0300

    a

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,61 +6,52 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 
 import java.io.*;
-public final class TaskImplementation implements FileEncoder
-{
+
+public final class TaskImplementation implements FileEncoder {
 
 
-    private static int toUnsigned(byte[] buffer)
-    {
-        int unsigned =0;
-        for(int i=0;i<3;++i)
-        {
+    private static int toUnsigned(byte[] buffer) {
+        int unsigned = 0;
+        for (int i = 0; i < 3; ++i) {
             int mid;
-            if(buffer[i]<0) mid=(buffer[i]&127)+128;
-            else mid=buffer[i];
-            mid=mid<<((2-i)*8);
-            unsigned+=mid;
+            if (buffer[i] < 0) mid = (buffer[i] & 127) + 128;
+            else mid = buffer[i];
+            mid = mid << ((2 - i) * 8);
+            unsigned += mid;
         }
         return unsigned;
     }
 
     @NotNull
-    public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException
-    {
-        final File fin = new File(finPath);
-        final File fout;
-
-        if (foutPath != null)
-        {
+    public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
+        File fout;
+        if (foutPath != null) {
             fout = new File(foutPath);
-        }
-        else
-        {
+        } else {
             fout = File.createTempFile("based_file_", ".txt");
             fout.deleteOnExit();
         }
-        FileInputStream fis = new FileInputStream(fin);
-        OutputStream os = new FileOutputStream(fout);
-
-        int s;
-        byte[] buffer=new byte[3];
-        s=fis.read(buffer);
-        int unsigned = toUnsigned(buffer);
-        int index;
-        while (s>=0)
-        {
-            for(int i=0;i<s+1;++i)
-            {
-                index=((unsigned<<(i*6+8))>>(18+8))&63;
-                os.write(toBase64[index]);
+        try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream(finPath));
+             BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(fout))) {
+            int s;
+            byte[] buffer = new byte[3];
+            s = fis.read(buffer);
+            int unsigned = toUnsigned(buffer);
+            int index;
+            while (s >= 0) {
+                for (int i = 0; i < s + 1; ++i) {
+                    index = ((unsigned << (i * 6 + 8)) >> (18 + 8)) & 63;
+                    fos.write(toBase64[index]);
+                }
+                for (int i = 0; i < 3 - s; ++i) {
+                    fos.write('=');
+                }
+                for (int i = 0; i < 3; ++i) buffer[i] = 0;
+                s = fis.read(buffer);
+                unsigned = toUnsigned(buffer);
             }
-            for(int i=0;i<3-s;++i)
-            {
-                os.write('=');
-            }
-            for(int i=0;i<3;++i) buffer[i]=0;
-            s=fis.read(buffer);
-            unsigned=toUnsigned(buffer);
+            fos.flush();
+            fos.close();
         }
         return fout;
     }
@@ -73,10 +64,9 @@ public final class TaskImplementation implements FileEncoder
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
     };
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         final FileEncoder encoder = new TaskImplementation();
-//         NOTE: open http://localhost:9000/ in your web browser
+        // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
 }
