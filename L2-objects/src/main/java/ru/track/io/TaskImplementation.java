bf502a63035940a14bb03adc6a2c37a551869c86
nLoro <Rudenko.N@outlook.com>
2017-10-19 02:51:06 +0300

    a

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -4,23 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
+import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 
 public final class TaskImplementation implements FileEncoder {
-
-
-    private static int toUnsigned(byte[] buffer) {
-        int unsigned = 0;
-        for (int i = 0; i < 3; ++i) {
-            int mid;
-            if (buffer[i] < 0) mid = (buffer[i] & 127) + 128;
-            else mid = buffer[i];
-            mid = mid << ((2 - i) * 8);
-            unsigned += mid;
-        }
-        return unsigned;
-    }
 
     /**
      * @param finPath  where to read binary data from
@@ -30,34 +19,8 @@ public final class TaskImplementation implements FileEncoder {
      */
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
-        File fout;
-        if (foutPath != null) {
-            fout = new File(foutPath);
-        } else {
-            fout = File.createTempFile("based_file_", ".txt");
-            fout.deleteOnExit();
-        }
-        try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream(finPath));
-             BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(fout))) {
-            int s;
-            byte[] buffer = new byte[3];
-            s = fis.read(buffer);
-            int unsigned = toUnsigned(buffer);
-            int index;
-            while (s >= 0) {
-                for (int i = 0; i < s + 1; ++i) {
-                    index = ((unsigned << (i * 6 + 8)) >> (18 + 8)) & 63;
-                    fos.write(toBase64[index]);
-                }
-                for (int i = 0; i < 3 - s; ++i) {
-                    fos.write('=');
-                }
-                for (int i = 0; i < 3; ++i) buffer[i] = 0;
-                s = fis.read(buffer);
-                unsigned = toUnsigned(buffer);
-            }
-        }
-        return fout;
+        /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
+        throw new UnsupportedOperationException(); // TODO: implement
     }
 
     private static final char[] toBase64 = {
@@ -69,8 +32,9 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new TaskImplementation();
+        final FileEncoder encoder = new ReferenceTaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
+
 }
--- L2-objects/src/test/java/ru/track/CountWordsTest.java
+++ L2-objects/src/test/java/ru/track/CountWordsTest.java
@@ -22,24 +22,24 @@ public class CountWordsTest {
         file = new File("words.txt");
     }
 
-//    @Test
-//    public void countNumbers() throws Exception {
-//
-//        CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
-//        long number = cw.countNumbers(file);
-//        String result = cw.concatWords(file);
-//
-//
-//        BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
-//        StringBuilder expected = new StringBuilder();
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            expected.append(line + " ");
-//        }
-//
-//        Assert.assertEquals("String concat: ", expected.toString(), result);
-//        Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
-//
-//    }
+    @Test
+    public void countNumbers() throws Exception {
+
+        CountWords cw = new CountWords("vu53f28MvpQ4PclHvxHZ");
+        long number = cw.countNumbers(file);
+        String result = cw.concatWords(file);
+
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("words_result.txt")));
+        StringBuilder expected = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            expected.append(line + " ");
+        }
+
+        Assert.assertEquals("String concat: ", expected.toString(), result);
+        Assert.assertEquals("Integer sum: ", (long) Integer.MAX_VALUE + 1, number);
+
+    }
 
 }
