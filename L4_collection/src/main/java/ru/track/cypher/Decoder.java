b204aec3abd67b6c4bd77be1fbb262276c446b78
DanielGonchar <dannygonchar@gmail.com>
2017-10-21 21:30:56 +0300

    accomplished cypher Decoder hometask

--- L4_collection/src/main/java/ru/track/cypher/Decoder.java
+++ L4_collection/src/main/java/ru/track/cypher/Decoder.java
@@ -1,7 +1,16 @@
 package ru.track.cypher;
 
 import java.util.LinkedHashMap;
+import java.util.*;
+import java.io.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -24,8 +33,15 @@ public class Decoder {
 
         cypher = new LinkedHashMap<>();
 
+        Iterator<Character> domainIt = domainHist.keySet().iterator();
+        Iterator<Character> encryptedDomainIt = encryptedDomainHist.keySet().iterator();
+        while(encryptedDomainIt.hasNext() && domainIt.hasNext()) {
+            cypher.put(encryptedDomainIt.next(), domainIt.next());
+        }
+
 
     }
+
 
     public Map<Character, Character> getCypher() {
         return cypher;
@@ -39,7 +55,20 @@ public class Decoder {
      */
     @NotNull
     public String decode(@NotNull String encoded) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for ( int i = 0 ; i < encoded.length(); i++){
+            char tmpChar = Character.toLowerCase(encoded.charAt(i));
+            if (tmpChar >= 'a' && tmpChar <= 'z')
+            {
+                sb.append(cypher.get(tmpChar));
+            }
+            else sb.append(tmpChar);
+
+        }
+
+        return sb.toString();
     }
 
     /**
@@ -53,7 +82,53 @@ public class Decoder {
      */
     @NotNull
     Map<Character, Integer> createHist(@NotNull String text) {
-        return null;
+
+        Map<Character,Integer> histMap = new HashMap<>();
+
+        for ( int i = 0 ; i < 26; i++){
+
+            histMap.put((char) (Character.valueOf('a') + i) ,0);
+
+        }
+
+        for( int i = 0; i < text.length(); i++){
+
+            char tmpChar = Character.toLowerCase(text.charAt(i));
+            if ((tmpChar >= 'a') && (tmpChar <= 'z'))
+            {
+                histMap.put(tmpChar,histMap.get(tmpChar)+1);
+            }
+
+        }
+
+        class MyComparator implements Comparator {
+
+            Map map;
+
+            public MyComparator(Map map) {
+                this.map = map;
+            }
+
+            public int compare(Object o1, Object o2) {
+
+                return ((Integer) map.get(o2)).compareTo((Integer) map.get(o1));
+
+            }
+        }
+
+        MyComparator comp = new MyComparator(histMap);
+
+        Map<Character, Integer> sortedMap = new TreeMap<>(comp);
+        sortedMap.putAll(histMap);
+
+        return sortedMap;
     }
 
+
 }
+
+
+
+
+
+
