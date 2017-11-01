3ca57c4164cf18d2cf60e99728b0b5170c5e6acb
NifantovaIrina <nifantova98@gmail.com>
2017-11-01 21:18:45 +0300

    It doesn't work!

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,14 +1,43 @@
 package ru.track.prefork;
 
+import java.awt.image.ImagingOpException;
+import java.io.*;
+import java.net.Socket;
+
 /**
  *
  */
 public class Client {
     private int port;
     private String host;
+    Socket clientSocket;
 
-    public Client(int port, String host) {
+    public Client(int port, String host) throws IOException {
         this.port = port;
         this.host = host;
+        clientSocket = new Socket(host, port);
+    }
+    public void connect() throws IOException {
+        OutputStream writer = clientSocket.getOutputStream();
+        writer.write(111);
+        InputStream reader = clientSocket.getInputStream();
+        int res = 1;
+        StringBuilder resp = new StringBuilder();
+        while (res != -1) {
+            byte[] query = new byte[1024];
+            res = reader.read(query);
+            resp.append(query);
+        }
+        System.out.println(resp.toString());
+        clientSocket.close();
+    }
+    public static void main(String[] args) {
+        try {
+            Client c = new Client(8080, "localhost");
+            c.connect();
+        }
+        catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,59 @@
 package ru.track.prefork;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.track.workers.NioClient;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
  */
 public class Server {
     private int port;
+    private ServerSocket socket;
+    private boolean stopped;
+    static Logger log = LoggerFactory.getLogger(NioClient.class);
 
-    public Server(int port) {
+    public Server(int port) throws IOException {
+        socket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
         this.port = port;
+    }
+
+    public void run() throws IOException {
+        while (!stopped) {
+            Socket client = socket.accept();
+            System.out.println("Accepted!");
+            OutputStream writer = client.getOutputStream();
+            InputStream reader = client.getInputStream();
+            int res = 1;
+            StringBuilder resp = new StringBuilder();
+            while (res != -1) {
+                byte[] query = new byte[1024];
+                res = reader.read(query);
+                resp.append(query);
+            }
+            writer.write(resp.toString().getBytes());
+//            System.out.println(query[0]);
+        }
+        socket.close();
+    }
+
+    public void close() {
+        stopped = true;
+    }
+    public static void main(String[] args) {
+        try {
+            Server s =  new Server(8080);
+            s.run();
+        }
+        catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+
+
     }
 }
