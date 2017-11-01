3d27ea129502a3dbe1f0fb13964742228bdc9ba4
Lizanoskova <enoskovamipt@gmail.com>
2017-11-01 21:31:44 +0300

    L7-echo-server

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,10 @@
 package ru.track.prefork;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  */
@@ -11,4 +16,31 @@ public class Client {
         this.port = port;
         this.host = host;
     }
+
+    public void loop() throws Exception {
+
+        Socket socket = new Socket(host, port);
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String line = scanner.nextLine();
+            out.write(line.getBytes());
+            out.flush();
+
+
+        byte[] buff = new byte[1024];
+        int reader = in.read(buff);
+        System.out.println(new String(buff, 0, reader));
+            }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        Client client = new Client(9000,"localhost");
+        client.loop();
+    }
+
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,57 @@
 package ru.track.prefork;
 
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  */
 public class Server {
+    static Logger log = LoggerFactory.getLogger(Server.class);
     private int port;
 
     public Server(int port) {
         this.port = port;
+    }
+
+    public void serve() throws IOException {
+        try (ServerSocket server = new ServerSocket(port, 10, InetAddress.getByName("localhost"))) {
+
+            Socket client = server.accept();
+            OutputStream out = client.getOutputStream();
+            InputStream in = client.getInputStream();
+            while(true)
+            {
+                byte[] buffer = new byte[1024];
+                int count = in.read(buffer);
+                log.info("Client:"+ new String(buffer,0,count));
+                String line = new String(buffer,0,count);
+                out.write(line.getBytes());
+                out.flush();
+            }
+
+        }
+        catch(IOException e)
+        {
+           log.error("ERROR" + e.getMessage()) ;
+        }
+        finally
+        {
+            if(client =! null)
+            {
+                client.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(9000);
+        server.serve();
     }
 }
