eacf05dfa03684ac1c1a4e81fc3db040ebdcb9fc
Dmitriy Goryachiy <goryachii@phystech.edu>
2017-11-01 21:18:47 +0300

    threads

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,10 @@
 package ru.track.prefork;
 
+import java.io.*;
+import java.net.Socket;
+
+import static java.lang.Thread.sleep;
+
 /**
  *
  */
@@ -10,5 +15,62 @@ public class Client {
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8000);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream ois = new DataInputStream(socket.getInputStream());
+
+
+        while (!socket.isOutputShutdown()) {
+            try {
+                if (br.ready()) {
+
+                    System.out.println("Client start writing in channel...");
+                    Thread.sleep(3000);
+                    String clientCommand = br.readLine();
+                    oos.writeUTF(clientCommand);
+                    oos.flush();
+                    System.out.println("Clien sent message " + clientCommand + " to server.");
+                    Thread.sleep(1000);
+
+                    if (clientCommand.equalsIgnoreCase("exit")) {
+                        System.out.println("Client kill connections");
+                        Thread.sleep(1000);
+                        System.out.println(ois.readUTF());
+                        if (ois.read() > -1) {
+                            System.out.println("reading...");
+                            String in = ois.readUTF();
+                            System.out.println(in);
+                            break;
+                        }
+                    }
+
+                }
+
+                //sleep(3000);
+
+                //System.out.println("w8ing for server answer");
+
+
+// если успел забираем ответ из канала сервера в сокете и сохраняем её в ois переменную,  печатаем на свою клиентскую консоль
+
+
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+            //System.out.println("azazazaz");
+//            if (ois.read() > -1) {
+//                System.out.println("reading...");
+//                String in = ois.readUTF();
+//                System.out.println(in);
+//            }
+        }
+
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,5 +1,13 @@
 package ru.track.prefork;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.Thread.sleep;
+
 /**
  *
  */
@@ -8,5 +16,69 @@ public class Server {
 
     public Server(int port) {
         this.port = port;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String entry;
+        while(true) {
+            try {
+                ServerSocket server = new ServerSocket(8000);
+                System.out.println("Server started");
+                Socket client = server.accept();
+
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                System.out.println("DataOutputStream  created");
+
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                System.out.println("DataInputStream created");
+
+
+                while (!client.isClosed()) {
+
+                    System.out.println("Server reading from channel");
+
+                    entry = in.readUTF();
+
+// после получения данных считывает их
+                    System.out.println("READ from client message - " + entry);
+
+// и выводит в консоль
+                    System.out.println("Server try writing to channel");
+                    //System.out.println(entry);
+// инициализация проверки условия продолжения работы с клиентом по этому сокету по кодовому слову       - quit
+                    if (entry.equalsIgnoreCase("exit")) {
+                        System.out.println("Client initialize connections suicide ...");
+                        out.writeUTF("Server reply - " + entry + " - OK");
+                        out.flush();
+//                        try {
+//                            //sleep(2000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        break;
+                    }
+
+                    //System.out.println(entry);
+                    out.writeUTF(entry);
+                    System.out.println("Server Wrote message to client.");
+                    System.out.println(entry);
+// освобождаем буфер сетевых сообщений (по умолчанию сообщение не сразу отправляется в сеть, а сначала накапливается в специальном буфере сообщений, размер которого определяется конкретными настройками в системе, а метод  - flush() отправляет сообщение не дожидаясь наполнения буфера согласно настройкам системы
+                    out.flush();
+                }
+
+// если условие выхода - верно выключаем соединения
+                System.out.println("Client disconnected");
+                System.out.println("Closing connections & channels.");
+
+                // закрываем сначала каналы сокета !
+                in.close();
+                out.close();
+
+                // потом закрываем сам сокет общения на стороне сервера!
+                //client.close();
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
