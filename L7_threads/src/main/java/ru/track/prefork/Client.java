5b5aeee2203ede5a8c715e97e96293bfa4abdc05
murnat98 <murnat98@yandex.ru>
2017-11-03 15:30:01 +0300

    server doing

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,11 @@
 package ru.track.prefork;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  */
@@ -10,5 +16,35 @@ public class Client {
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+
+    private void connect() throws IOException {
+        Socket socket = new Socket(host, port);
+
+        OutputStream outputStream = socket.getOutputStream();
+        InputStream inputStream = socket.getInputStream();
+
+        byte[] response = new byte[1024];
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String message = scanner.nextLine();
+
+            outputStream.write(message.getBytes());
+            inputStream.read(response);
+
+            System.out.println(new String(response));
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = null;
+
+        try {
+            client = new Client(8080, "127.0.0.1");
+            client.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,70 @@
 package ru.track.prefork;
 
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  */
 public class Server {
+    private static Logger logger = LoggerFactory.getLogger("logger");
     private int port;
+    private static final int MAX_COUNT = 1024;
 
     public Server(int port) {
         this.port = port;
+    }
+
+    private void serve() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+
+        logger.info("new client connected");
+
+        byte[] bytes = new byte[MAX_COUNT];
+
+        while (true)
+        {
+            if (socket.isClosed()) {
+                socket = serverSocket.accept();
+
+                logger.info("new client connected");
+            }
+
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+
+            int readBytesCount = inputStream.read(bytes);
+            if (readBytesCount == -1) {
+                socket.close();
+            }
+            logger.info("read " + readBytesCount + " bytes from client");
+
+            outputStream.write(bytes);
+            logger.info("written bytes to client");
+
+            if (!socket.isBound()) {
+                socket.close();
+
+                logger.info("connection with client closed");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = null;
+        try {
+            server = new Server(8080);
+            server.serve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
