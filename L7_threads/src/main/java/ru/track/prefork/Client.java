d362db7405c0c2ad55ec5c17d9e5cc97177abd09
Sharapov Roman <sharapov.roman@gmail.com>
2017-11-06 01:31:31 +0300

    make first, second and third points

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,14 +1,116 @@
 package ru.track.prefork;
+import java.security.Signature;
+import java.util.Scanner;
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.track.workers.NioClient;
+import java.io.*;
+import java.net.Socket;
 
 /**
  *
  */
 public class Client {
+    private boolean myError;
+    static Logger log = LoggerFactory.getLogger(Client.class);
     private int port;
     private String host;
+    Socket socket;
 
-    public Client(int port, String host) {
+    public Client( @NotNull String host, int port) {
+        this.myError = false;
         this.port = port;
         this.host = host;
+        this.socket = null;
+
     }
+
+
+    public void workInCycle() {
+
+
+        try {
+            socket = new Socket(String.valueOf(host), port);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        myClientReader myClientReader = new myClientReader();
+        myClientReader.start();
+
+        try (
+                Scanner myScanner = new Scanner(System.in);
+                OutputStream out = socket.getOutputStream();
+        ) {
+            while (true) {
+                String myNextLine = myScanner.nextLine();
+                if (myExit(myNextLine))
+                    myError = true;
+                if (myError) {
+                    break;
+                }
+                out.write(myNextLine.getBytes());
+                out.flush();
+
+            }
+        } catch (Exception e) {
+            myError = true;
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    private boolean myExit(String message) {
+        if (message.equals("exit"))
+            return true;
+        return false;
+    }
+
+    private class myClientReader extends Thread {
+
+        InputStream myStream;
+
+        private myClientReader() {
+            myStream = null;
+        }
+
+        @Override
+        public void run() {
+            myClientReaderFunc();
+        }
+
+        private void myClientReaderFunc() {
+            try {
+                while (true) {
+                    myStream = socket.getInputStream();
+                    byte[] myBuffer = new byte[1024];
+                    int amountOfBytes = myStream.read(myBuffer);
+                    if ( myError || !(amountOfBytes >= 0) ) {
+                        log.info("Error has occured! Please contact adminstrator");
+                        myError = true;
+                        break;
+                    }
+                    System.out.println(new String(myBuffer, 0, amountOfBytes));
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                myError = true;
+            }
+            finally {
+                try {
+                    myStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Client myClient = new Client("localhost", 8000);
+        myClient.workInCycle();
+    }
+
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,216 @@
 package ru.track.prefork;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-/**
- *
- */
+
+
 public class Server {
+    private int nextWorkerID;
     private int port;
+
+    private Map<Integer, Worker> mappedWorkerID;
+    private static Logger log = LoggerFactory.getLogger(Server.class);
+
 
     public Server(int port) {
         this.port = port;
+        this.nextWorkerID = 0;
+        this.mappedWorkerID = new LinkedHashMap<>();
+    }
+
+
+    private class Worker extends Thread {
+        private boolean sessionIsInterrupted;
+        private myServerReader mySingleWorker;
+        private myWriter singleWriter;
+        private Socket socket;
+        private int id;
+
+
+        private class myServerReader  extends Thread {
+            private InputStream myStream;
+
+            private myServerReader () {
+                myStream = null;
+                sessionIsInterrupted = false;
+            }
+
+
+            @Override
+            public void run() {
+                readerFunc ();
+            }
+
+            private void readerFunc () {
+                byte[] buffer = new byte[1024];
+                try {
+                    myStream = socket.getInputStream();
+                    while (true) {
+                        int amountOfBytes = myStream.read(buffer);
+                        if (amountOfBytes > 0) {
+                            System.out.println(this.getName() + "> " + new String(buffer, 0, amountOfBytes));
+                            broadcastMailing(buffer, amountOfBytes, id);
+                        } else {
+                            sessionIsInterrupted = true;
+                            log.info("Lost connection to server! Please contact your administrator.");
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        myStream.close();
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        private void myWorkProcess () {
+
+            try {
+                log.info("Client has connected");
+                mySingleWorker = new myServerReader ();
+                singleWriter = new myWriter();
+                singleWriter.setName(this.getName() + "_Writer");
+                mySingleWorker .setName(this.getName() + "_Client");
+                mySingleWorker .start();
+                singleWriter.run();
+                while (true) {
+                    if (sessionIsInterrupted) {
+                        break;
+                    }
+                    else continue;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                mappedWorkerID.remove(id);
+            }
+            return;
+
+        }
+
+        private class myWriter extends Thread {
+
+            private OutputStream outputStream;
+
+            private void sendMessagesg(byte[] msg, int amountOfBytes) throws IOException {
+
+                outputStream.write(msg, 0, amountOfBytes);
+                outputStream.flush();
+            }
+
+            private myWriter() {
+
+                outputStream = null;
+            }
+
+            @Override
+            public void run() {
+                writeProcess();
+            }
+
+            private void writeProcess() {
+                try {
+                    outputStream = socket.getOutputStream();
+                    while (true) {
+
+                        if (sessionIsInterrupted)
+                            break;
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+                finally {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+        private Worker(Socket socket, int id) {
+            this.socket = socket;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            myWorkProcess ();
+        }
+
+
+
+    }
+
+    private void broadcastMailing(@NotNull byte[] message, @NotNull int amountOfBytes, @Nullable int sendersID) {
+        log.info("Start broadcasting " + new String(message, 0, amountOfBytes));
+        for (Integer id : mappedWorkerID.keySet()) {
+            if (id.equals(sendersID))
+                continue;
+
+            try {
+                mappedWorkerID.get(id).singleWriter.sendMessagesg(message, amountOfBytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void workInCycle() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
+        } catch (Exception e) {
+            System.out.println("Server hasn't started. Following error occured:\n" + e.getMessage());
+            return;
+        }
+        System.out.println("Server is runnig!\n");
+
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Worker myAnotherWorker = new Worker(socket, nextWorkerID);
+                mappedWorkerID.put(nextWorkerID, myAnotherWorker);
+                myAnotherWorker.setName(String.format("Client%d@%s:%s", nextWorkerID++, socket.getInetAddress(), socket.getPort()));
+                myAnotherWorker.start();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+
+            try {
+                serverSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8000);
+        server.workInCycle();
     }
 }
