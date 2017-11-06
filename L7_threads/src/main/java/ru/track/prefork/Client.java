bec2a3752b7a0d2875bc0b300f9bb373f1482650
unknown <vasili4396@mail.ru>
2017-11-06 10:03:25 +0300

    upd

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,17 @@
 package ru.track.prefork;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 /**
  *
  */
@@ -10,5 +22,42 @@ public class Client {
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+    public void loop() throws Exception {
+        Socket socket = null;
+        Logger log = LoggerFactory.getLogger(Server.class);
+        BufferedReader fromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            socket = new Socket(host, port);
+        } catch (UnknownHostException uhe) {
+            log.error ("cant open socket");
+            System.exit(-1);
+        }
+
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+
+        byte [] buffer = new byte [1024];
+        String str;
+
+        while (true){
+            str = fromKeyboard.readLine();
+            if (!str.equals("")) {
+                out.write(str.getBytes());
+                out.flush();
+                int nRead = in.read(buffer);
+                str = new String(buffer, 0, nRead);
+                if (str.equals("exit"))
+                    break;
+                else
+                    System.out.println(str);
+            }
+            else;
+        }
+    }
+
+    public static void main (String[] args) throws Exception {
+        Client client = new Client(9000, "localhost");
+        client.loop();
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,114 @@
 package ru.track.prefork;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.mail.internet.InternetAddress;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import static java.lang.System.exit;
+import static java.lang.System.in;
+import static java.lang.System.setOut;
+
 /**
  *
  */
 public class Server {
     private int port;
+    static Logger log = LoggerFactory.getLogger(Server.class);
+    //    OutputStream out = null;
+//    InputStream in = null;
+    static ArrayList clientOutputStreams;
 
-    public Server(int port) {
+    public Server(int port) throws IOException {
         this.port = port;
+    }
+
+    public class MultiThreadServer implements Runnable {
+        Socket clientSocket = null;
+
+        public MultiThreadServer(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            ServerSocket serverSocket = null;
+            BufferedReader clientInput = null;
+
+            try {
+                clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String msgToClients;
+            try {
+                while ((msgToClients = clientInput.readLine()) != null) {
+                    log.info("message from client" + msgToClients);
+                    tellEveryone(msgToClients);
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            try {
+                serverSocket = new ServerSocket(9000, 10, InetAddress.getByName("localhost"));
+                log.info("Server is working");
+            } catch (IOException ioe) {
+                log.error("Can't raise server");
+                ioe.printStackTrace();
+                exit(-1);
+            }
+
+            try {
+                clientSocket = serverSocket.accept();
+                log.info("Got new client" + clientSocket.getPort());
+            } catch (IOException ioe) {
+                log.error("Client cant connect");
+                ioe.printStackTrace();
+                exit(-1);
+            }
+//            try {
+//                in = clientSocket.getInputStream();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                out = clientSocket.getOutputStream();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public void serve() throws Exception {
+        byte[] buffer = new byte[1024];
+        int nRead;
+        while (true) try {
+            nRead = in.read(buffer);
+            if (nRead == 0) ;
+            else {
+                String str = new String(buffer, 0, nRead);
+                out.write(str.getBytes());
+                out.flush();
+            }
+        } catch (Exception ex) {
+            //log.error("smth's gone wrong");
+        }
+        }
+    }
+
+    public static void main (String[] args) throws Exception {
+        Server server = new Server(9000);
+        server.runserver();
+        server.serve();
     }
 }
