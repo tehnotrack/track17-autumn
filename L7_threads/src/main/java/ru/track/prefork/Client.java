e88d07fb066f2d9cf2405d666bdfa778aa6392e3
Kiill Bukin <kirill2718@mail.ru>
2017-11-08 17:04:24 +0300

    Выполнены пункты 1-3

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,14 @@
 package ru.track.prefork;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 /**
  *
  */
@@ -10,5 +19,66 @@ public class Client {
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+
+    private void listen(OutputStream os, InputStream is) {
+        try {
+            byte[] buffer = new byte[4096];
+            int nRead;
+            while (true) {
+                nRead = is.read(buffer);
+                if (nRead == -1) {
+                    break;
+                }
+                System.out.println(new String(buffer, 0, nRead));
+            }
+        }
+        catch(IOException e) {}
+        finally {
+            IOUtils.closeQuietly(os);
+            IOUtils.closeQuietly(is);
+            System.out.println("Соединение с сервером разорвано");
+        }
+    }
+
+    private void send(OutputStream os, InputStream is) {
+        try {
+            Scanner in = new Scanner(System.in);
+            String str;
+            while (true) {
+                str = in.nextLine();
+                if (str.equals("exit")) break;
+                if (str.length() > 0) {
+                    os.write(str.getBytes());
+                    os.flush();
+                }
+            }
+        }
+        catch(IOException e) {}
+        finally{
+            IOUtils.closeQuietly(os);
+            IOUtils.closeQuietly(is);
+        }
+    }
+
+    public void start() throws IOException {
+        Socket sock = null;
+        Thread t;
+        try {
+            sock = new Socket(host, port);
+            final OutputStream os = sock.getOutputStream();
+            final InputStream is = sock.getInputStream();
+            t = new Thread(() -> {send(os, is);});
+            t.setDaemon(true);
+            t.start();
+            listen(os, is);
+        }
+        finally {
+            IOUtils.closeQuietly(sock);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        final Client client = new Client(8100,"localhost");
+        client.start();
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,103 @@
 package ru.track.prefork;
 
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+
 /**
  *
  */
 public class Server {
     private int port;
+    private AtomicLong counter;
+    private ConcurrentMap<Long, String> messages;
+    Object sync = new Object();
 
     public Server(int port) {
         this.port = port;
+        counter = new AtomicLong(0);
+        messages = new ConcurrentHashMap<>();
+    }
+
+    private void handle(@NotNull Socket sock){
+        try {
+            long id = counter.getAndIncrement();
+            Thread.currentThread().setName("Client[" + id + "]@"+sock.getInetAddress().toString()+":"+sock.getPort());
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            System.out.println("Подключился: " + Thread.currentThread().getName());
+            InputStream is = sock.getInputStream();
+            OutputStream os = sock.getOutputStream();
+            new Thread(() -> {listen(is, id, "Client@"+sock.getInetAddress().toString()+":"+sock.getPort()+"> ", Thread.currentThread());}).start();
+            synchronized(sync) {
+                try {
+                    while (true) {
+                        sync.wait();
+                        for (ConcurrentMap.Entry<Long, String> entry : messages.entrySet())
+                            if (!entry.getKey().equals(id))
+                                os.write(entry.getValue().getBytes());
+                    }
+                } catch (InterruptedException e) {}
+            }
+        }
+        catch(IOException e) {}
+    }
+
+    private void listen(InputStream is, long id, String name, Thread t){
+        try {
+            Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+            int nRead;
+            byte[] buffer = new byte[1024];
+            while (true) {
+                nRead = is.read(buffer);
+                if (nRead > -1) {
+                    String str = new String(buffer, 0, nRead);
+                    System.out.println(name + str);
+                    synchronized(sync){
+                        messages.clear();
+                        messages.put(id, name + str);
+                        sync.notifyAll();
+                    }
+                } else break;
+            }
+        }
+        catch(IOException e) {}
+        finally{
+            System.out.println("Client["+ id + "] отсоединился");
+            t.interrupt();
+        }
+    }
+
+    public void serve() throws IOException{
+        ServerSocket ssock = null;
+        try {
+            ssock = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
+            Socket sock;
+            Thread t;
+            while (true) {
+                sock = ssock.accept();
+                final Socket socket = sock;
+                t = new Thread(() -> {handle(socket);});
+                t.start();
+            }
+        }
+        finally {
+            IOUtils.closeQuietly(ssock);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Server server = new Server(8100);
+        server.serve();
     }
 }
