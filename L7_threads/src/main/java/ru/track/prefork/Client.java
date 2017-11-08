6e6c50b2edcc35fc1791aba8e509d23ca2cabef8
Arseny Kuskov <kuskov.as@phystech.edu>
2017-11-08 23:17:56 +0300

    1,2 task imp

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,14 +1,77 @@
 package ru.track.prefork;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  *
  */
 public class Client {
     private int port;
     private String host;
+    private Socket socket;
 
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+        if(runClient() == 0) {
+            clientHandler();
+        }
+    }
+
+    private int runClient() {
+        try {
+            System.out.println("[main]: Try to connect to server.");
+            socket = new Socket(this.host, this.port);
+            System.out.println("[main]: Successfully connected.");
+        } catch (IOException e) {
+            System.out.println("[main]: Error while connection.");
+            return(-1);
+        }
+        return 0;
+    }
+
+    private void clientHandler() {
+        System.out.println("[main]: Try to send HELO:");
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream ois = new DataInputStream(socket.getInputStream())
+        ) {
+            Thread readerThread = new Thread(() -> {
+                    while(!socket.isOutputShutdown() && !Thread.interrupted()) {
+                        try {
+                            System.out.println(Thread.currentThread().getName() + ": Reading...");
+                            String response = ois.readUTF();
+                            System.out.println(Thread.currentThread().getName() + ": Response read.");
+                            System.out.println(Thread.currentThread().getName() + ": I have read: " + response);
+                        } catch (IOException e) {
+                            System.out.println(Thread.currentThread().getName() + ": Can't read.");
+                            break;
+                        }
+                    }
+            });
+
+            readerThread.start();
+
+            while(!socket.isOutputShutdown()) {
+                if(br.ready()) {
+                    String data = br.readLine();
+                    oos.writeUTF(data);
+                    oos.flush();
+                    if(data.equalsIgnoreCase("exit")) {
+                        readerThread.interrupt();
+                        break;
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("[main]: Can't send HELO.");
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Client client = new Client(8080, "localhost");
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,146 @@
 package ru.track.prefork;
 
+
+import javax.xml.bind.Element;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+
 /**
  *
  */
 public class Server {
     private int port;
+    private boolean isRunning = false;
+    private ServerSocket socket;
+    private AtomicLong counter = new AtomicLong(0);
+    private ExecutorService pool = null;
+    private ConcurrentHashMap<Long, Thread> threadMap;
 
-    public Server(int port) {
+
+    private Server(int port, ExecutorService pool) {
         this.port = port;
+        this.isRunning = true;
+        this.pool = pool;
+        this.threadMap = new ConcurrentHashMap();
+        if(startServer() == 0) {
+            listenLoop();
+        }
+    }
+
+    private int startServer() {
+        try {
+            System.out.println("[main]: Try to start server.");
+            this.socket = new ServerSocket(this.port);
+            System.out.println("[main]: Server started at port " + this.port + ".");
+        } catch (IOException e) {
+            System.out.println("[main]: Can't start socket. Error: " + e.getLocalizedMessage());
+            return(-1);
+        } catch (NullPointerException e) {
+            System.out.println("[main]: Can't start socket. Error: " + e.getLocalizedMessage());
+        }
+        return 0;
+    }
+
+    private void listenLoop() {
+        while (isRunning) {
+            pool.execute(new Handler(this.socket));
+        }
+    }
+
+    public class Handler implements Runnable {
+
+        private ServerSocket socket;
+        private DataInputStream in = null;
+        private DataOutputStream out = null;
+
+        private Handler(ServerSocket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                String oldName = Thread.currentThread().getName();
+                System.out.println(String.format("[%s]: Wait for connection.", Thread.currentThread().getName()));
+                Socket client = this.socket.accept();
+
+                Thread.currentThread().setName(String.format("Client[%d]@[%s]:[%d]", counter.incrementAndGet(),
+                        client.getInetAddress().toString(),
+                        client.getPort()));
+
+
+                System.out.println(String.format("[%s]: Client accepted.", Thread.currentThread().getName()));
+
+                this.out = new DataOutputStream(client.getOutputStream());
+                this.in = new DataInputStream(client.getInputStream());
+
+                threadMap.put(counter.get(), Thread.currentThread());
+
+                while(!client.isClosed()) {
+                    String data = in.readUTF();
+                    System.out.println(String.format("[%s]: Client write: " + data + ".",
+                            Thread.currentThread().getName()));
+                    if(data.equals("exit")) {
+                        break;
+                    }
+
+                    out.writeUTF(data); //# HELO Server;
+
+//                    for(Map.Entry<Long, Thread> entry : threadMap.entrySet()) {
+//                        entry.getValue();
+//                    }
+
+                    out.flush();
+                    System.out.println(String.format("[%s]: Sent: \"" + data + "\" to client.",
+                            Thread.currentThread().getName()));
+
+                }
+                out.close();
+                in.close();
+                client.close();
+                System.out.println(String.format("[%s]: Close connection.",
+                        Thread.currentThread().getName()));
+                Thread.currentThread().setName(oldName);
+                threadMap.remove(counter.get());
+            } catch (IOException e) {
+                System.out.println(String.format("[%s]: Error while new client acceptance.",
+                        Thread.currentThread().getName()));
+            }
+        }
+
+        public void sendMessage(String message) {
+            try {
+                this.out.writeUTF(message);
+                this.out.flush();
+            } catch (IOException e) {
+                System.out.println(String.format("[%s]: Error while sending message.",
+                        Thread.currentThread().getName()));
+            }
+
+        }
+
+        @Override
+        public String toString(){
+            return Thread.currentThread().getName();
+        }
+    }
+
+
+    public static void main(String[] args) throws Throwable {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 10, 0L,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue(2));
+
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        Server server = new Server(8080, pool);
     }
 }
+
+
