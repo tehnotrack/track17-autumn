0ad7a9c42b48a78edfb3458fee8f195cf6a858f2
NifantovaIrina <nifantova98@gmail.com>
2017-11-08 21:29:05 +0300

    1-2 implementation

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -12,41 +12,29 @@ import java.util.Scanner;
 public class Client {
     private int port;
     private String host;
-    Socket clientSocket;
+    private Socket clientSocket;
 
-    public Client(int port, String host) throws IOException {
+    private Client(int port, String host) throws IOException {
         this.port = port;
         this.host = host;
         clientSocket = new Socket(host, port);
     }
-    public static String read(Socket clientSocket) throws IOException{
-        InputStream reader = clientSocket.getInputStream();
-        int len = 0;
-        StringBuilder resp = new StringBuilder();
-        while (len != -1) {
-            byte[] query = new byte[1024];
-            len = reader.read(query);
-            if (len != -1) {
-                byte [] subArr = Arrays.copyOfRange(query, 0, len);
-                String buf = new String(subArr);
-                resp.append(buf);
-            }
-        }
-        return resp.toString();
-    }
+
+
 
     public void connect() throws IOException {
-        OutputStream writer = clientSocket.getOutputStream();
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        writer.write(str.getBytes());
-        writer.flush();
-        clientSocket.shutdownOutput();
+        String currStr = in.nextLine();
+        OutputStream writer = clientSocket.getOutputStream();
+        while (!currStr.equals("exit")) {
+            writer.write(currStr.getBytes());
+            writer.flush();
+            currStr = in.nextLine();
+        }
 
-        String resp = read(clientSocket);
-        System.out.println("Server answered: " + resp);
         clientSocket.close();
     }
+
     public static void main(String[] args) {
         try {
             Client c = new Client(8080, "localhost");
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -5,35 +5,70 @@ import ru.track.workers.NioClient;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  */
+
 public class Server {
     private int port;
     private ServerSocket socket;
     private boolean stopped;
     static Logger log = LoggerFactory.getLogger(NioClient.class);
+    public class Worker implements Runnable {
+        Socket client;
+        Worker(Socket client) {
+            this.client = client;
 
+        }
+
+        public void run() {
+            try {
+                String name = Thread.currentThread().getName();
+                System.out.printf("%s started\n", name);
+                InputStream reader = client.getInputStream();
+                OutputStream writer = client.getOutputStream();
+                int len;
+                byte[] buf = new byte[1024];
+                while((len = reader.read(buf)) >= 0) {
+                    writer.write(buf, 0, len);
+                    byte[] copy = Arrays.copyOfRange(buf,0, len);
+                    String temp = new String(copy);
+                    System.out.println(temp);
+                }
+                reader.close();
+                client.close();
+            }
+            catch (IOException ex) {
+
+            }
+
+        }
+    }
     public Server(int port) throws IOException {
         socket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
         this.port = port;
     }
 
+
     public void run() throws IOException {
+        AtomicInteger clientsCounter = new AtomicInteger();
         while (!stopped) {
             Socket client = socket.accept();
-            System.out.println("Accepted!");
-            OutputStream writer = client.getOutputStream();
-            String resp = Client.read(client);
-            System.out.println("Client: " + resp);
-            writer.write(resp.getBytes());
-            client.shutdownOutput();
+            Thread thread = new Thread(new Worker(client),
+                    String.format("Client[%d]@%s:%s", clientsCounter.addAndGet(1),
+                            client.getLocalAddress(),
+                            client.getLocalPort()));
+
+            thread.start();
+
         }
         socket.close();
     }
