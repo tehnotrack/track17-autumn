8493572918ef09615ce048123b6a433264ec4c1d
Andrew Gonchar <biohazard100000@gmail.com>
2017-11-15 18:56:00 +0300

    threads 1) done

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,9 @@
 package ru.track.prefork;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  */
@@ -10,5 +14,31 @@ public class Client {
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client(9876, "localhost");
+        client.loop();
+    }
+
+    private void loop() {
+        try (
+                Socket echoSocket = new Socket(host, port);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(
+                        new InputStreamReader(System.in));
+                ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                if (userInput.equals("exit")) break;
+                System.out.printf("Echo: %s%n", in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,49 @@
 package ru.track.prefork;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
 /**
  *
  */
 public class Server {
     private int port;
+    static Logger log = LoggerFactory.getLogger(Server.class);
 
     public Server(int port) {
         this.port = port;
+    }
+
+    public void serve() {
+        try (
+                ServerSocket listener = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
+                Socket clientSocket = listener.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream())
+                )
+                ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.equals("exit")) break;
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // log.info("Client: " + new String(buffer, 0, nRead));
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(9876);
+        server.serve();
     }
 }
