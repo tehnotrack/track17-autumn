fe4bb1743148cd8da35f0ded4464b91bfc36917a
Vyacheslav Kovalev <vyacheslav.kovalev@phystech.edu>
2017-11-19 22:50:18 +0300

    echo server

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,14 +1,72 @@
 package ru.track.prefork;
 
-/**
- *
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Client {
+    static Logger log = LoggerFactory.getLogger(Client.class);
     private int port;
     private String host;
 
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client(9000, "localhost");
+        try {
+            client.loop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loop() throws Exception {
+        System.out.println("Client started!");
+        Socket socket = new Socket(host, port);
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            while (true) {
+                // read line
+                System.out.println("Write msg for server:");
+                String line = scanner.next();
+
+                // write to socket
+                out.write(line.getBytes());
+                out.flush();
+
+                // condition to close socket
+                if (line.equals("exit")) {
+                    socket.close();
+                    return;
+                }
+
+                // read msg from server
+                byte[] buffer = new byte[1024];
+                int nRead = in.read(buffer);
+
+                // print msg from server
+                byte[] slicedBuffer = Arrays.copyOfRange(buffer, 0, nRead);
+                String msgFromServer = new String(slicedBuffer);
+                log.info("From server: " + msgFromServer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            out.write("exit".getBytes());
+            out.flush();
+            socket.close();
+        }
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,68 @@
 package ru.track.prefork;
 
-/**
- *
- */
+import org.slf4j.Logger;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+
 public class Server {
+    static Logger log = org.slf4j.LoggerFactory.getLogger(Server.class);
     private int port;
 
     public Server(int port) {
         this.port = port;
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(9000);
+        try {
+            server.serve();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void serve() throws Exception {
+        System.out.println("Server started!");
+        ServerSocket serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
+        while (true) {
+            // new client
+            Socket socket = serverSocket.accept();
+            log.info("Accepted: " + socket.getPort());
+
+            // streams
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+
+            while (true) {
+                // read from client socket
+                byte[] buffer = new byte[1024];
+                int nRead = in.read(buffer);
+                if (nRead < 1) {
+                    break;
+                }
+
+                // write echo to client
+                out.write(buffer, 0, nRead);
+                out.flush();
+
+                // print msg from client
+                byte[] slicedBuffer = Arrays.copyOfRange(buffer, 0, nRead);
+                String msgFromClient = new String(slicedBuffer);
+                System.out.println("Client" + socket.getPort() + ":" + msgFromClient);
+
+                // condition to close connection
+                if (msgFromClient.equals("exit")) {
+                    break;
+                }
+            }
+            socket.close();
+            log.info("Disconnected: " + socket.getPort());
+        }
     }
 }
