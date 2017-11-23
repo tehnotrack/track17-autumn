cb562749794a196f20458340e797b444064227c3
Vyacheslav Kovalev <vyacheslav.kovalev@phystech.edu>
2017-11-23 13:51:12 +0300

    multithreaded server fix

--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,21 +1,30 @@
 package ru.track.prefork;
 
+import com.sun.mail.iap.Protocol;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ProtocolException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 public class Server {
-    static Logger log = org.slf4j.LoggerFactory.getLogger(Server.class);
+    static Logger log = LoggerFactory.getLogger(Server.class);
+
     private int port;
+    private AtomicLong serverCounter = new AtomicLong(0);
+//    private Protocol<String> protocol;
 
     public Server(int port) {
         this.port = port;
+//        this.protocol = protocol;
     }
 
     public static void main(String[] args) {
@@ -31,65 +40,56 @@ public class Server {
         System.out.println("Server started!");
 
         ServerSocket serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
-        int socketNum = 0;
         while (!serverSocket.isClosed()) {
-            Socket clientSocket = serverSocket.accept();
+            final Socket socket = serverSocket.accept();
 
-            Runnable worker = (new Worker()).init(clientSocket, socketNum++);
-
-            Thread thread = new Thread(worker);
-            thread.setName("Client[" + socketNum + "]" + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+            Thread thread = new Thread(() -> {
+                try {
+                    log.info("Connected");
+                    handleSocket(socket);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            thread.setName(String.format("Client[%d]@%s:%d", serverCounter.getAndIncrement(), socket.getInetAddress(), socket.getPort()));
             thread.start();
-            log.info("New client accepted: " + thread.getName());
         }
     }
 
-    class Worker implements Runnable {
-        Socket socket;
-        int socketId; // connection counter
+    private void handleSocket(Socket socket) throws IOException, ProtocolException {
+        try (OutputStream out = socket.getOutputStream();
+             InputStream in = socket.getInputStream();) {
 
-        @Override
-        public void run() {
-            try (OutputStream out = socket.getOutputStream();
-                 InputStream in = socket.getInputStream();) {
-
-                while (!socket.isClosed()) {
-                    // read from client socket
-                    byte[] buffer = new byte[1024];
-                    int nbytes = in.read(buffer);
-                    if (nbytes < 1) {
-                        break;
-                    }
-
-                    // write echo to client
-                    out.write(buffer, 0, nbytes);
-                    out.flush();
-
-                    // print msg from client
-                    String msgFromClient = new String(buffer, 0, nbytes);
-                    log.info("Message: " + msgFromClient);
-
-                    // condition to close connection
-                    if (msgFromClient.equals("exit")) {
-                        break;
-                    }
+            while (!socket.isClosed()) {
+                // read from client socket
+                byte[] buffer = new byte[1024];
+                int nbytes = in.read(buffer);
+                if (nbytes < 1) {
+                    break;
                 }
-            } catch (IOException e) {
-                log.error("IOException", e);
-            } finally {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    log.error("Can't close client socket. " + e);
+
+                // write echo to client
+                out.write(buffer, 0, nbytes);
+                out.flush();
+
+                // print msg from client
+                String msgFromClient = new String(buffer, 0, nbytes);
+                log.info("Message: " + msgFromClient);
+
+                // condition to close connection
+                if (msgFromClient.equals("exit")) {
+                    break;
                 }
-                log.info("Client disconnected");
             }
-        }
-
-        public Runnable init(Socket socket, int socketId) {
-            this.socket = socket;
-            this.socketId = socketId;
-            return (this);
+        } catch (IOException e) {
+            log.error("IOException", e);
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                log.error("Can't close client socket. " + e);
+            }
+            log.info("Client disconnected");
         }
     }
 }
