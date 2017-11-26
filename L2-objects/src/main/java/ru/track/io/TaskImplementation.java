9f2b82235521b2c41f4593e97a9b9fd16b66f4cd
RobertVoropaev <robertvoropaev11@gmail.com>
2017-11-26 03:41:46 +0300

    Changed the characted output to byte

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -25,7 +25,8 @@ public final class TaskImplementation implements FileEncoder {
         if (foutPath != null) {
             fout = new File(foutPath);
         } else {
-            fout = File.createTempFile("based_file_", ".txt");
+            fout = new File("temp_file.txt");
+            fout.createNewFile();
             fout.deleteOnExit();
         }
         byte[] in = new byte[(int)fin.length()];
@@ -36,18 +37,26 @@ public final class TaskImplementation implements FileEncoder {
             while((c=reader.read()) != -1){
                 in[i++] = (byte)c;
             }
+            reader.close();
         }
-        try(FileWriter writer = new FileWriter(fout))
+
+        try(BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(fout)))
         {
-            byte[] test = new byte[]{(byte) 0xd8, (byte) 0xff, (byte) 0xe0, (byte) 0xff};
             writer.write(Base64(in));
             writer.flush();
         }
         return fout;
     }
 
-    private static String Base64(byte[] in){
-        StringBuilder res = new StringBuilder();
+    private static byte[] Base64(byte[] in){
+        int len = in.length;
+        if (len%3 == 1) {
+            len += 2;
+        } else if (len%3 == 2) {
+            len += 1;
+        }
+        byte[] res = new byte[len * 4 / 3];
+        int j = 0;
         for(int i = 2; i < in.length; i+=3){
             byte byte1 = in[i - 2];
             byte byte2 = in[i - 1];
@@ -58,24 +67,23 @@ public final class TaskImplementation implements FileEncoder {
             byte newByte3 = (byte)((byte2 << 2) & 0b111100 | (byte3 >> 6) & 0b000011);
             byte newByte4 = (byte)(byte3 & 0b111111);
 
-            res.append(toBase64[newByte1])
-                    .append(toBase64[newByte2])
-                    .append(toBase64[newByte3])
-                    .append(toBase64[newByte4]);
+            res[j++] = (byte) toBase64[newByte1];
+            res[j++] = (byte) toBase64[newByte2];
+            res[j++] = (byte) toBase64[newByte3];
+            res[j++] = (byte) toBase64[newByte4];
         }
+
         if(in.length % 3 == 1){
             byte byte1 = in[in.length - 1];
 
             byte newByte1 = (byte)((byte1 >> 2) & 0b111111);
             byte newByte2 = (byte)((byte1 << 4) & 0b110000);
 
-            res.append(toBase64[newByte1])
-                    .append(toBase64[newByte2])
-                    .append('=')
-                    .append('=');
-        }
-
-        else if(in.length % 3 == 2){
+            res[j++] = (byte) toBase64[newByte1];
+            res[j++] = (byte) toBase64[newByte2];
+            res[j++] = (byte)('=');
+            res[j++] = (byte)('=');
+        } else if(in.length % 3 == 2){
             byte byte1 = in[in.length - 1];
             byte byte2 = in[in.length - 1];
 
@@ -83,12 +91,12 @@ public final class TaskImplementation implements FileEncoder {
             byte newByte2 = (byte)((byte1 << 4) & 0b110000 | (byte2 >> 4) & 0b001111);
             byte newByte3 = (byte)((byte2 << 2) & 0b111100);
 
-            res.append(toBase64[newByte1])
-                    .append(toBase64[newByte2])
-                    .append(toBase64[newByte3])
-                    .append('=');
+            res[j++] = (byte) toBase64[newByte1];
+            res[j++] = (byte) toBase64[newByte2];
+            res[j++] = (byte) toBase64[newByte3];
+            res[j++] = (byte)('=');
         }
-        return res.toString();
+        return res;
     }
 
     private static final char[] toBase64 = {
@@ -102,7 +110,7 @@ public final class TaskImplementation implements FileEncoder {
     public static void main(String[] args) throws IOException {
         final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
-        new Bootstrapper(args, encoder).bootstrap(9090);
+        new Bootstrapper(args, encoder).bootstrap(9000);
     }
 
 }
