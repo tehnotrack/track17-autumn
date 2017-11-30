a6dd1825213458c8e23bc670184435e23e9cf537
S3tmefree <bloya@mail.ru>
2017-11-30 21:30:11 +0300

    task threads

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,5 +1,8 @@
 package ru.track.prefork;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  *
  */
@@ -11,4 +14,38 @@ public class Client {
         this.port = port;
         this.host = host;
     }
+
+    public void createConnection() {
+        try (Socket socket = new Socket("localhost", 5000);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            String  outstr;
+            while (!socket.isClosed() && ((outstr = stdin.readLine()) != null)) {
+                //чтение
+                if (outstr.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                else //произвольная строка
+                {
+                    out.println(outstr);
+                }
+                //запись
+                String s = in.readLine();
+                if (!s.isEmpty()) {
+                    System.out.println(s);
+                }
+            }
+        }
+        catch(java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client(5000, "localhost");
+        client.createConnection();
+    }
+
 }
