ef8d825f3cac9a1091cce535a5bc5dec36fca98b
alexpol11 <alexpol11@mail.ru>
2017-12-05 15:24:53 +0300

    base_64

--- L2-objects/src/main/java/ru/track/io/TaskImplementation.java
+++ L2-objects/src/main/java/ru/track/io/TaskImplementation.java
@@ -6,8 +6,8 @@ import ru.track.io.vendor.Bootstrapper;
 import ru.track.io.vendor.FileEncoder;
 import ru.track.io.vendor.ReferenceTaskImplementation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public final class TaskImplementation implements FileEncoder {
 
@@ -17,10 +17,52 @@ public final class TaskImplementation implements FileEncoder {
      * @return file to read encoded data from
      * @throws IOException is case of input/output errors
      */
+
     @NotNull
     public File encodeFile(@NotNull String finPath, @Nullable String foutPath) throws IOException {
         /* XXX: https://docs.oracle.com/javase/8/docs/api/java/io/File.html#deleteOnExit-- */
-        throw new UnsupportedOperationException(); // TODO: implement
+
+        File finFile = new File(finPath);
+        File foutFile = new File(foutPath == null ? "./createdFile.txt" : foutPath);
+
+        try (
+            BufferedInputStream reader = new BufferedInputStream(new FileInputStream(finPath));
+            FileWriter writer = new FileWriter(foutFile);
+        ) {
+            byte[] byteArray = new byte[3];
+
+            int readed = reader.read(byteArray, 0, 3);
+            int i = 0;
+            int tmp3Char = 0;
+
+            while (readed >= 0) {
+                i = 0;
+                do {
+                    if (i == readed) {
+                        break;
+                    }
+                    tmp3Char |= (byteArray[i] >= 0 ? byteArray[i] : (255 & byteArray[i])) << (8 * (2 - i));
+                    i++;
+                } while (i != 3);
+
+                int j = 0;
+                while (j != i + 1) {
+                    writer.write(toBase64[(tmp3Char & (0x3f << (6 * (3 - j)))) >>> 6 * (3 - j)]);
+                    j++;
+                }
+                j = 0;
+                while (j < 3 - readed) {
+                    writer.write('=');
+                    j++;
+                }
+                tmp3Char = 0;
+
+                readed = reader.read(byteArray, 0, 3);
+            }
+
+            return foutFile;
+        }
+//        throw new UnsupportedOperationException(); // TODO: implement
     }
 
     private static final char[] toBase64 = {
@@ -32,8 +74,11 @@ public final class TaskImplementation implements FileEncoder {
     };
 
     public static void main(String[] args) throws IOException {
-        final FileEncoder encoder = new ReferenceTaskImplementation();
+//        final FileEncoder encoder = new ReferenceTaskImplementation();
+        final FileEncoder encoder = new TaskImplementation();
         // NOTE: open http://localhost:9000/ in your web browser
+//        String[] argc = {"/Users/alexander/Desktop/Track/Java/gitfolder/track17-autumn/L2-objects/image_256.png","/Users/alexander/Desktop/Track/Java/gitfolder/track17-autumn/L2-objects/image_1112.txt"};
+
         new Bootstrapper(args, encoder).bootstrap(9000);
     }
 
