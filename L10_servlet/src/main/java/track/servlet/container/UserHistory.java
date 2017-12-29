8220e24cf4f4ee90353014a2c8942c7ab9011729
alexpol11 <alexpol11@mail.ru>
2017-12-29 14:22:18 +0300

    servlet без лишних файлов

---
+++ L10_servlet/src/main/java/track/servlet/container/UserHistory.java
@@ -0,0 +1,29 @@
+package track.servlet.container;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UserHistory extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> params = req.getParameterMap();
+
+        if (params.containsKey("limit") && params.containsKey("username")){
+            ConversationService conv = new ConversationService();
+
+            List<Message> msgUserHistory = conv.getByUser(req.getParameter("username"), Long.parseLong(req.getParameter("limit")));
+            req.setAttribute("msgList", msgUserHistory);
+            req.getRequestDispatcher("/history.jsp").forward(req, resp);
+        }else {
+            resp.setContentType("text/html");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("<h1>Sorry, but u haven't entered limit or username field(</h1>");
+        }
+    }
+}
